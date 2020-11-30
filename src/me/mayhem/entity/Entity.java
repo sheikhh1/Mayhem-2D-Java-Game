748c677c202cd382f506e71fd0890e21a7e291be
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-11-30 13:48:14 +0000

    Removed Drawable - changed mind & added methods to Entity interface

--- src/me/mayhem/entity/Entity.java
+++ src/me/mayhem/entity/Entity.java
@@ -1,13 +1,18 @@
 package me.mayhem.entity;
 
+import org.jsfml.graphics.RenderWindow;
 import org.jsfml.system.Vector2f;
 
 public interface Entity {
 
     int getId();
 
+    int getPriority();
+
     Vector2f getPosition();
 
     void setPosition(Vector2f position);
+
+    void draw(RenderWindow window);
 
 }
