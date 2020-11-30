1d6c97f1eb48b1481fb2d1a8b4382e47c5b32462
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-11-30 13:50:26 +0000

    Added priority to AbstractEntity

--- src/me/mayhem/entity/AbstractEntity.java
+++ src/me/mayhem/entity/AbstractEntity.java
@@ -5,21 +5,28 @@ import org.jsfml.system.Vector2f;
 public abstract class AbstractEntity implements Entity {
 
     private final int id;
+    private final int priority;
 
     private Vector2f position;
 
     public AbstractEntity(int id) {
-        this(id, new Vector2f(0, 0));
+        this(id, 0, new Vector2f(0, 0));
     }
 
-    public AbstractEntity(int id, Vector2f position) {
+    public AbstractEntity(int id, int priority, Vector2f position) {
         this.id = id;
+        this.priority = priority;
         this.position = position;
     }
 
     @Override
     public int getId() {
         return this.id;
+    }
+
+    @Override
+    public int getPriority() {
+        return this.priority;
     }
 
     @Override
