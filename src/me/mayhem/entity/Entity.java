88ae44a4c754bbba37f4d69703e727e27d1113b0
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-08 20:20:32 +0000

    Added position and motion variables and getters

--- src/me/mayhem/entity/Entity.java
+++ src/me/mayhem/entity/Entity.java
@@ -1,4 +1,18 @@
 package me.mayhem.entity;
 
+import me.mayhem.math.Vector;
+
 public class Entity {
+
+    private Vector position;
+    private Vector motion;
+
+    public Vector getPosition() {
+        return this.position;
+    }
+
+    public Vector getMotion() {
+        return this.motion;
+    }
+
 }
