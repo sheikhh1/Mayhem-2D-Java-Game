c599bd9bd78d9cf1293a9b5fd9b77347641da3df
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-08 20:51:20 +0000

    Added pathing getter to entity class

--- src/me/mayhem/entity/Entity.java
+++ src/me/mayhem/entity/Entity.java
@@ -1,11 +1,13 @@
 package me.mayhem.entity;
 
+import me.mayhem.entity.pathing.Pathing;
 import me.mayhem.math.Vector;
 
 public class Entity {
 
     private Vector position;
     private Vector motion;
+    private Pathing pathing;
 
     public Vector getPosition() {
         return this.position;
@@ -14,5 +16,7 @@ public class Entity {
     public Vector getMotion() {
         return this.motion;
     }
+
+    public Pathing getPathing() {return this.pathing;}
 
 }
