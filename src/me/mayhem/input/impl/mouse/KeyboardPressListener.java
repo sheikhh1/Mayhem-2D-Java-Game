2d97f9b1b5038d444bd54246bc74addd2dbe5739
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-08 23:42:42 +0000

    Added keyboard listeners

---
+++ src/me/mayhem/input/impl/mouse/KeyboardPressListener.java
@@ -0,0 +1,12 @@
+package me.mayhem.input.impl.mouse;
+
+import me.mayhem.input.InputListener;
+import org.jsfml.window.event.Event;
+import org.jsfml.window.event.KeyEvent;
+
+public abstract class KeyboardPressListener extends InputListener<KeyEvent> {
+
+    public KeyboardPressListener() {
+        super(Event.Type.KEY_PRESSED);
+    }
+}
