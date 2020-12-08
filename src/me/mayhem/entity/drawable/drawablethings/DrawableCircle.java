557fca7e21df90a662966e0c213b581105a6beab
j-sammon <j.sammon@lancaster.ac.uk>
2020-12-08 20:40:41 +0000

    adding javadoc

--- src/me/mayhem/entity/drawable/drawablethings/DrawableCircle.java
+++ src/me/mayhem/entity/drawable/drawablethings/DrawableCircle.java
@@ -3,12 +3,24 @@ package me.mayhem.entity.drawable.drawablethings;
 import org.jsfml.graphics.CircleShape;
 import org.jsfml.graphics.RenderWindow;
 
+/**
+ * draws a circle on a canvas
+ */
 public class DrawableCircle {
     private CircleShape circle;
 
+    /**
+     *
+     * @param circle the circle to draw
+     */
     public DrawableCircle(CircleShape circle) {
         this.circle = circle;
     }
+
+    /**
+     *
+     * @param canvas the canvas to draw onto
+     */
     public void Draw(RenderWindow canvas){
         canvas.draw(circle);
     }
