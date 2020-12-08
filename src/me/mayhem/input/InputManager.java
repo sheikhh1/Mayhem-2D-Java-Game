862b4d0260eac4b5d703b106abcaa7aa66ec33f3
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-08 23:33:57 +0000

    Updated InputManager for new list

--- src/me/mayhem/input/InputManager.java
+++ src/me/mayhem/input/InputManager.java
@@ -6,13 +6,15 @@ import java.util.*;
 
 public class InputManager {
 
-    private static Map<Event.Type, List<InputListener>> registeredListeners = new HashMap<>();
+    private static Map<Event.Type, List<InputListener<?>>> registeredListeners = new HashMap<>();
 
-    public static void registerInput(InputListener listener) {
-        registeredListeners.computeIfAbsent(listener.getType(), ___ -> new ArrayList<>()).add(listener);
+    public static void registerInput(InputListener<?> listener) {
+        for (Event.Type type : listener.getTypes()) {
+            registeredListeners.computeIfAbsent(type, ___ -> new ArrayList<>()).add(listener);
+        }
     }
 
-    public static List<InputListener> getListeners(Event.Type type) {
+    public static List<InputListener<?>> getListeners(Event.Type type) {
         return registeredListeners.getOrDefault(type, Collections.emptyList());
     }
 }
