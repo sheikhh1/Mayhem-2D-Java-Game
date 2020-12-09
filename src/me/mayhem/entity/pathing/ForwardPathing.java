1c80f514e0148e1fb09ddc4298ed7869a45275f4
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-09 14:28:20 +0000

    Added a FowardPathing class

---
+++ src/me/mayhem/entity/pathing/ForwardPathing.java
@@ -0,0 +1,13 @@
+package me.mayhem.entity.pathing;
+
+import me.mayhem.entity.Entity;
+
+/**
+ * Required to make an entity move forward - (Positive X Axis)
+ */
+public class ForwardPathing implements Pathing{
+    @Override
+    public void updatePosition(Entity entity) {
+        entity.getMotion().add(1,0);
+    }
+}
