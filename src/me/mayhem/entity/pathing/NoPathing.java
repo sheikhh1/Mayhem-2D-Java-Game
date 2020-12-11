4af4e41bc0306de8438d13ebbb3a1c66133fec38
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 15:58:22 +0000

    Refactor

--- src/me/mayhem/entity/pathing/NoPathing.java
+++ src/me/mayhem/entity/pathing/NoPathing.java
@@ -5,7 +5,7 @@ import me.mayhem.entity.Entity;
 /**
  * NoPathing required for the player entity as AI is not needed
  */
-public class NoPathing implements Pathing{
+public class NoPathing implements Pathing {
 
     @Override
     public void updatePosition(Entity entity) {
