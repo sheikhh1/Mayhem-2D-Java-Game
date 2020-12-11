5c8ddf2146115204116f1b42fde32bfdba77d800
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 17:10:03 +0000

    Added FloatAttribute

---
+++ src/me/mayhem/attribute/type/FloatAttribute.java
@@ -0,0 +1,10 @@
+package me.mayhem.attribute.type;
+
+import me.mayhem.attribute.AbstractAttribute;
+
+public class FloatAttribute extends AbstractAttribute<Float> {
+
+    public FloatAttribute(String identifier, Float value) {
+        super(identifier, value);
+    }
+}
