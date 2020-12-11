3470a5a3e14f2252c952145c1d11a790cbbac715
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 17:09:41 +0000

    Added StringAttribute

---
+++ src/me/mayhem/attribute/type/StringAttribute.java
@@ -0,0 +1,10 @@
+package me.mayhem.attribute.type;
+
+import me.mayhem.attribute.AbstractAttribute;
+
+public class StringAttribute extends AbstractAttribute<String> {
+
+    public StringAttribute(String identifier, String value) {
+        super(identifier, value);
+    }
+}
