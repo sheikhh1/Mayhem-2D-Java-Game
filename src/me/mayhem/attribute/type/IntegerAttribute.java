412148d6c3c25bf217a1a9abade5611deceff422
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 17:09:17 +0000

    Added IntegerAttribute

---
+++ src/me/mayhem/attribute/type/IntegerAttribute.java
@@ -0,0 +1,10 @@
+package me.mayhem.attribute.type;
+
+import me.mayhem.attribute.AbstractAttribute;
+
+public class IntegerAttribute extends AbstractAttribute<Integer> {
+
+    public IntegerAttribute(String identifier, Integer value) {
+        super(identifier, value);
+    }
+}
