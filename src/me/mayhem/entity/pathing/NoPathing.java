a6fa5e56db6b0c1c6367a009ed85071abf361c8e
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-11 15:52:39 +0000

    Added NoPathing class for Player Entity

---
+++ src/me/mayhem/entity/pathing/NoPathing.java
@@ -0,0 +1,14 @@
+package me.mayhem.entity.pathing;
+
+import me.mayhem.entity.Entity;
+
+/**
+ * NoPathing required for the player entity as AI is not needed
+ */
+public class NoPathing implements Pathing{
+
+    @Override
+    public void updatePosition(Entity entity) {
+        // Do nothing for player
+    }
+}
