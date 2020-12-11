f88ec253ec6ee43461e110cd1485ee8f44b5fdd7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 17:10:47 +0000

    Added BooleanAttribute

---
+++ src/me/mayhem/attribute/type/BooleanAttribute.java
@@ -0,0 +1,10 @@
+package me.mayhem.attribute.type;
+
+import me.mayhem.attribute.AbstractAttribute;
+
+public class BooleanAttribute extends AbstractAttribute<Boolean> {
+
+    public BooleanAttribute(String identifier, Boolean value) {
+        super(identifier, value);
+    }
+}
