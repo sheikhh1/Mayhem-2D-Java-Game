fa3f6ba3bafe2caeb66f2f1edbd7c1f9bcd0a563
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 17:09:10 +0000

    Added AbstractAttribute

---
+++ src/me/mayhem/attribute/AbstractAttribute.java
@@ -0,0 +1,27 @@
+package me.mayhem.attribute;
+
+public abstract class AbstractAttribute<T> implements Attribute<T> {
+
+    private final String identifier;
+    private T value;
+
+    public AbstractAttribute(String identifier, T value) {
+        this.identifier = identifier;
+        this.value = value;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    @Override
+    public T getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
