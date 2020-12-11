d77d781e53109dc4ba08e7a01d4cc791c94212e1
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2020-12-11 17:10:24 +0000

    Added DoubleAttribute

---
+++ src/me/mayhem/attribute/type/DoubleAttribute.java
@@ -0,0 +1,10 @@
+package me.mayhem.attribute.type;
+
+import me.mayhem.attribute.AbstractAttribute;
+
+public class DoubleAttribute extends AbstractAttribute<Double> {
+
+    public DoubleAttribute(String identifier, Double value) {
+        super(identifier, value);
+    }
+}
