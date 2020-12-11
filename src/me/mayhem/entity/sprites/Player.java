cf5f3e53df03d76541b1855dca9faac936417f9a
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-11 18:15:25 +0000

    Commented Player Class

--- src/me/mayhem/entity/sprites/Player.java
+++ src/me/mayhem/entity/sprites/Player.java
@@ -7,16 +7,20 @@ import me.mayhem.math.Vector;
 
 /**
  * Player Class
- *
  */
 public class Player extends Entity {
 
     private String name;
 
+    /**
+     * Player Constructor
+     * @param name - Name of Player (Assigned by user)
+     * @param position - Current position of the player
+     */
     public Player(String name, Vector position) {
         super(EntityType.PLAYER, position, Vector.ZERO, new NoPathing());
 
-        this.name = name;
+        this.name = name; // Name assigned and stored
     }
 
     public String getName() {
