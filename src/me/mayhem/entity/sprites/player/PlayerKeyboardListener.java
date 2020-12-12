0c1be490613cf81d8dadfa7c353a3fd5186e0fcc
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-12 18:49:52 +0000

    Created a Player Mouse Input Listener

--- src/me/mayhem/entity/sprites/player/PlayerKeyboardListener.java
+++ src/me/mayhem/entity/sprites/player/PlayerKeyboardListener.java
@@ -1,18 +1,18 @@
 package me.mayhem.entity.sprites.player;
 
 import me.mayhem.input.impl.keyboard.KeyboardInputListener;
+import me.mayhem.input.impl.keyboard.KeyboardPressListener;
 import org.jsfml.window.event.KeyEvent;
 
 /**
  * Keyboard input listener for the Player Entity
  */
-public class PlayerKeyboardListener extends KeyboardInputListener {
+public class PlayerKeyboardListener extends KeyboardPressListener {
 
     @Override
     protected void takeInput(KeyEvent event) {
         System.out.println("Player Pressed:" + event.asKeyEvent().key);
 
-        //TODO: Add functionality for W,A,S,D,Space..
-        //TODO: Keys registering twice?
+        //TODO: Add functionality for W,A,S,D,Space
     }
 }
---
+++ src/me/mayhem/entity/sprites/player/PlayerMouseListener.java
@@ -0,0 +1,15 @@
+package me.mayhem.entity.sprites.player;
+
+import me.mayhem.input.impl.mouse.MouseButtonPressListener;
+import org.jsfml.window.event.MouseButtonEvent;
+
+/**
+ * Mouse Listener for the Player Entity
+ */
+public class PlayerMouseListener extends MouseButtonPressListener {
+
+    @Override
+    protected void takeInput(MouseButtonEvent event) {
+        System.out.println("Player has pressed the Mouse");
+    }
+}
