960a9c807b4c8280f049316111c80595ac11b565
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-12 13:14:17 +0000

    Created a Block Class & Added a Block constructor

---
+++ src/me/mayhem/level/Block.java
@@ -0,0 +1,14 @@
+package me.mayhem.level;
+
+public class Block {
+
+    public int x,y;
+    public int width, height;
+
+    public Block (int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
