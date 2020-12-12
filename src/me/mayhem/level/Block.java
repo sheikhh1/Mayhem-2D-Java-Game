352762e5685eeead88c72a37f441366f11d7061d
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-12 13:15:55 +0000

    Commented Block Class

--- src/me/mayhem/level/Block.java
+++ src/me/mayhem/level/Block.java
@@ -5,6 +5,13 @@ public class Block {
     public int x,y;
     public int width, height;
 
+    /**
+     * Block Constructor
+     * @param x - X position of block
+     * @param y - Y position of block
+     * @param width - Width of block
+     * @param height - Height of block
+     */
     public Block (int x, int y, int width, int height){
         this.x = x;
         this.y = y;
