cf764129f761653ad28f915dbe34fefab81d2b81
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-13 16:57:05 +0000

    Split key inputs into case statements

--- src/me/mayhem/entity/sprites/player/PlayerKeyboardListener.java
+++ src/me/mayhem/entity/sprites/player/PlayerKeyboardListener.java
@@ -2,6 +2,7 @@ package me.mayhem.entity.sprites.player;
 
 import me.mayhem.input.impl.keyboard.KeyboardInputListener;
 import me.mayhem.input.impl.keyboard.KeyboardPressListener;
+import org.jsfml.window.Keyboard;
 import org.jsfml.window.event.KeyEvent;
 
 /**
@@ -11,8 +12,23 @@ public class PlayerKeyboardListener extends KeyboardPressListener {
 
     @Override
     protected void takeInput(KeyEvent event) {
-        System.out.println("Player Pressed:" + event.asKeyEvent().key);
+        //TODO: Add functionality for W,A,S,D
+        // Fight Button?
 
-        //TODO: Add functionality for W,A,S,D,Space
+
+        switch(event.asKeyEvent().key){
+            case W: // Replace with SPACE?
+                System.out.println("Jump");
+                break;
+            case A:
+                System.out.println("Move Back");
+                break;
+            case S: // Pick up button?
+                System.out.println("S is pressed");
+                break;
+            case D:
+                System.out.println("Move Forward");
+                break;
+        }
     }
 }
