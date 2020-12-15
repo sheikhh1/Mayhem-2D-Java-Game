be482adefae77dd1bd0c6bb3c0676644aacd1b39
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-15 18:36:26 +0000

    Refactored - Added basic player movement functionality

--- src/me/mayhem/entity/sprites/player/PlayerKeyboardInputListener.java
+++ src/me/mayhem/entity/sprites/player/PlayerKeyboardInputListener.java
@@ -1,33 +1,40 @@
 package me.mayhem.entity.sprites.player;
 
-import me.mayhem.input.impl.keyboard.KeyboardInputListener;
 import me.mayhem.input.impl.keyboard.KeyboardPressListener;
-import org.jsfml.window.Keyboard;
 import org.jsfml.window.event.KeyEvent;
 
 /**
  * Keyboard input listener for the Player Entity
  */
-public class PlayerKeyboardListener extends KeyboardPressListener {
+public class PlayerKeyboardInputListener extends KeyboardPressListener {
+
+    private Player player;
+
+    public PlayerKeyboardInputListener(Player p){
+        this.player = p;
+    }
 
     @Override
     protected void takeInput(KeyEvent event) {
         //TODO: Add functionality for W,A,S,D
         // Fight Button?
 
-
         switch(event.asKeyEvent().key){
             case W: // Replace with SPACE?
                 System.out.println("Jump");
+                player.setVelY(-5f);
                 break;
             case A:
                 System.out.println("Move Back");
+                player.setVelX(-5f);
                 break;
             case S: // Pick up button?
                 System.out.println("S is pressed");
+                player.setVelY(5f);
                 break;
             case D:
                 System.out.println("Move Forward");
+                player.setVelX(5f);
                 break;
         }
     }
