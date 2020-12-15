280139c6599264cb77e0c6aeabfd8745b5650f5d
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-15 18:36:44 +0000

    Added Player Object to screen

--- src/me/mayhem/Mayhem.java
+++ src/me/mayhem/Mayhem.java
@@ -1,9 +1,11 @@
 package me.mayhem;
 
-import me.mayhem.entity.sprites.player.PlayerKeyboardListener;
+import me.mayhem.entity.sprites.player.Player;
+import me.mayhem.entity.sprites.player.PlayerKeyboardInputListener;
 import me.mayhem.entity.sprites.player.PlayerMouseListener;
 import me.mayhem.input.InputListener;
 import me.mayhem.input.InputManager;
+import me.mayhem.math.Vector;
 import org.jsfml.graphics.Color;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.window.VideoMode;
@@ -14,17 +16,17 @@ public class Mayhem {
     public static void main(String[] args) {
         RenderWindow window = new RenderWindow();
         window.create(new VideoMode(640, 480), "Mayhem");
-
+        Player myPlayer = new Player("test", new Vector(100,100));
         window.setFramerateLimit(30);
 
         new PlayerMouseListener();
-        new PlayerKeyboardListener();
+        new PlayerKeyboardInputListener(myPlayer);
 
         while (window.isOpen()) {
             window.clear(Color.WHITE);
 
             //TODO: tick drawables
-
+            myPlayer.update(window); // Draw Player onto window
             window.display();
 
             for (Event event : window.pollEvents()) {
@@ -36,7 +38,6 @@ public class Mayhem {
                     }
                 }
             }
-
             //TODO: tick objects/entities
         }
     }
