396e80959f1d6daec8815c57f681805021251940
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-15 18:35:18 +0000

    Updated Player.java
    - Created X & Y directional velocties
    - Added a update method to update the position of the player

--- src/me/mayhem/entity/sprites/player/Player.java
+++ src/me/mayhem/entity/sprites/player/Player.java
@@ -4,6 +4,11 @@ import me.mayhem.entity.Entity;
 import me.mayhem.entity.EntityType;
 import me.mayhem.entity.pathing.impl.NoPathing;
 import me.mayhem.math.Vector;
+import org.jsfml.graphics.Color;
+import org.jsfml.graphics.RectangleShape;
+import org.jsfml.graphics.RenderWindow;
+
+import java.awt.*;
 
 /**
  * Player Class
@@ -11,6 +16,12 @@ import me.mayhem.math.Vector;
 public class Player extends Entity {
 
     private String name;
+    private Vector position;
+
+    //TODO: Implement DrawableSprite
+    private RectangleShape test = new RectangleShape(new Vector(50,50).toVector());
+    private float velX = 0f;
+    private float velY = 0f;
 
     /**
      * Player Constructor
@@ -21,9 +32,44 @@ public class Player extends Entity {
         super(EntityType.PLAYER, position, Vector.ZERO, new NoPathing());
 
         this.name = name; // Name assigned and stored
+        this.position = position;
+
+        test.setFillColor(Color.BLUE);
+        test.setScale(1,1);
+
     }
 
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Velocity in X Direction
+     * @param x
+     */
+    public void setVelX(float x) {
+        this.velX = x;
+        this.position.add(velX,0);
+    }
+
+    /**
+     * Velocity in Y Direction
+     * @param y
+     */
+    public void setVelY(float y){
+        this.velY = y;
+        this.position.add(0,velY);
+
+    }
+
+    /**
+     * Updates position of the Player
+     * @param rend
+     */
+    public void update(RenderWindow rend){
+        test.setPosition(this.position.toVector());
+        rend.draw(test);
+    }
+
+
 }
