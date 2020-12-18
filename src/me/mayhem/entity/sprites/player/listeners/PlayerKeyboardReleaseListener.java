c9a13ea9c1770cfab1f03e4cc193f823c4938bbf
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-18 19:10:59 +0000

    Replaced case statement with map

--- src/me/mayhem/entity/sprites/player/listeners/PlayerKeyboardReleaseListener.java
+++ src/me/mayhem/entity/sprites/player/listeners/PlayerKeyboardReleaseListener.java
@@ -1,27 +1,33 @@
 package me.mayhem.entity.sprites.player.listeners;
 
 import me.mayhem.entity.sprites.player.Player;
+import me.mayhem.entity.sprites.player.PlayerState;
 import me.mayhem.input.impl.keyboard.KeyboardReleaseListener;
+import org.jsfml.window.Keyboard;
 import org.jsfml.window.event.KeyEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Handles events on key releases by the Player entity
  */
 public class PlayerKeyboardReleaseListener extends KeyboardReleaseListener {
-    Player player;
+    private Player player;
+    private Map<Keyboard.Key, PlayerState> keyReleaseHandler = new HashMap<>();
+
+
 
     public PlayerKeyboardReleaseListener(Player player) {
         this.player = player;
+
+        keyReleaseHandler.put(Keyboard.Key.A,PlayerState.STANDING);
+        keyReleaseHandler.put(Keyboard.Key.D,PlayerState.STANDING);
     }
 
     @Override
     protected void takeInput(KeyEvent event) {
-        switch (event.asKeyEvent().key) {
-            case A:
-            case D:
-                player.setState(Player.playerState.STANDING);
-                break;
-        }
+        player.setState(keyReleaseHandler.get(event.asKeyEvent().key));
 
     }
 }
