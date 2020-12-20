fbe74fbcacb3a67302f3bd9b9b6f37a06f959fcd
danorris709 <dnorris443@gmail.com>
2020-12-20 21:39:07 +0000

    Added variables to Difficulty filled with null placeholders

--- src/me/mayhem/game/level/difficulty/Difficulty.java
+++ src/me/mayhem/game/level/difficulty/Difficulty.java
@@ -1,13 +1,29 @@
 package me.mayhem.game.level.difficulty;
 
+import me.mayhem.game.level.layout.ai.LevelGenerator;
+import me.mayhem.game.level.spawning.EntitySpawner;
+
 public enum Difficulty {
 
-    EASY(),
-    MEDIUM(),
-    HARD(),
+    EASY(null, null),
+    MEDIUM(null, null),
+    HARD(null, null),
 
     ;
 
-    ///TODO: add some attributes to make level generation more difficult
+    private final LevelGenerator generator;
+    private final EntitySpawner spawner;
 
+    Difficulty(LevelGenerator generator, EntitySpawner spawner) {
+        this.generator = generator;
+        this.spawner = spawner;
+    }
+
+    public LevelGenerator getGenerator() {
+        return this.generator;
+    }
+
+    public EntitySpawner getSpawner() {
+        return this.spawner;
+    }
 }
