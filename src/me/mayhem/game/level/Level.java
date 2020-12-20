94a01ac45faa7ee932fbe0c9fea5281cb00cdba4
danorris709 <dnorris443@gmail.com>
2020-12-20 21:37:41 +0000

    Added variables and getters

--- src/me/mayhem/game/level/Level.java
+++ src/me/mayhem/game/level/Level.java
@@ -2,19 +2,27 @@ package me.mayhem.game.level;
 
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.player.Player;
+import me.mayhem.game.level.difficulty.Difficulty;
+import me.mayhem.game.level.layout.Layout;
+import me.mayhem.game.level.spawning.EntitySpawner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Level {
 
-    //Difficult, Spawner and Layout
     private final List<Entity> entities = new ArrayList<>();
 
     private final Player player;
+    private final Difficulty difficulty;
+    private final EntitySpawner spawner;
+    private final Layout layout;
 
-    public Level() {
+    public Level(Difficulty difficulty) {
         this.player = this.spawnPlayer();
+        this.difficulty = difficulty;
+        this.spawner = null; //TODO: get from difficulty
+        this.layout = null; //TODO: create layer
     }
 
     private Player spawnPlayer() {
@@ -27,5 +35,17 @@ public class Level {
 
     public Player getPlayer() {
         return this.player;
+    }
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+    public EntitySpawner getSpawner() {
+        return this.spawner;
+    }
+
+    public Layout getLayout() {
+        return this.layout;
     }
 }
