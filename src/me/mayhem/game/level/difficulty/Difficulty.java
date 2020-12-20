efd887559374e8834cad2c597ceeb866e0ee5685
danorris709 <dnorris443@gmail.com>
2020-12-20 21:31:34 +0000

    Added Difficulty enum

---
+++ src/me/mayhem/game/level/difficulty/Difficulty.java
@@ -0,0 +1,13 @@
+package me.mayhem.game.level.difficulty;
+
+public enum Difficulty {
+
+    EASY(),
+    MEDIUM(),
+    HARD(),
+
+    ;
+
+    ///TODO: add some attributes to make level generation more difficult
+
+}
