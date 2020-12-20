41f4ce3d0e8fcea5d167884523714dfb8793b633
sheikhh1 <haarissheikh2000@gmail.com>
2020-12-20 14:35:22 +0000

    Created PlayerAnimation.java
    - Will handle the animations when the player is preforming certain moves

---
+++ src/me/mayhem/entity/sprites/player/animation/PlayerAnimation.java
@@ -0,0 +1,17 @@
+package me.mayhem.entity.sprites.player.animation;
+
+
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
+
+public class PlayerAnimation {
+
+    private Texture playerTexture = new Texture();
+    private Sprite playerSprite = new Sprite(playerTexture);
+
+    public PlayerAnimation() {
+
+    }
+}
+
+     
