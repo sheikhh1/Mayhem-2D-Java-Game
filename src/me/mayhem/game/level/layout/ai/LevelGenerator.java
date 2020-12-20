6bae8893ce62146d54163e5419498e8d9637c2c4
danorris709 <dnorris443@gmail.com>
2020-12-20 21:42:17 +0000

    Added basics to Layout

--- src/me/mayhem/game/level/layout/ai/LevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/LevelGenerator.java
@@ -2,8 +2,10 @@ package me.mayhem.game.level.layout.ai;
 
 import me.mayhem.game.level.layout.block.Block;
 
+import java.util.List;
+
 public interface LevelGenerator {
 
-    Block[][] generateLevel();
+    List<Block> generateLevel();
 
 }
