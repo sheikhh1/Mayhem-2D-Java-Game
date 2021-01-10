7c7864964ad1b5a39167356ba9c1fd9b83f7a001
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-10 10:38:51 +0000

    comment abstractHitbox

--- src/me/mayhem/screens/homepage/HomePageManager.java
+++ src/me/mayhem/screens/homepage/HomePageManager.java
@@ -1,4 +1,17 @@
 package me.mayhem.screens.homepage;
 
-public class HomePageManager {
+import me.mayhem.screens.ScreenManager;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+
+public class HomePageManager implements ScreenManager {
+    @Override
+    public void loadScreen(RenderWindow renderWindow) {
+        renderWindow.create(new VideoMode(640, 480), "Mayhem");
+    }
+
+    @Override
+    public void unloadScreen(RenderWindow renderWindow) {
+        renderWindow.close();
+    }
 }
