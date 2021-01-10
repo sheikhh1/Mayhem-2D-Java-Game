ca4936fba6f8735d8410bbbabbd2b3e06ff56368
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-10 10:55:37 +0000

    Added Player key listeners to GameManager

--- src/me/mayhem/game/GameManager.java
+++ src/me/mayhem/game/GameManager.java
@@ -1,25 +1,36 @@
 package me.mayhem.game;
 
+import me.mayhem.game.entity.player.Player;
+import me.mayhem.game.entity.player.listeners.PlayerKeyboardPressListener;
+import me.mayhem.game.entity.player.listeners.PlayerKeyboardReleaseListener;
+import me.mayhem.game.entity.player.listeners.PlayerMousePressListener;
+import me.mayhem.math.Vector;
 import org.jsfml.graphics.RenderWindow;
 
 public class GameManager {
 
     private final RenderWindow renderWindow;
+    private Player myPlayer = new Player("test", new Vector(100,100));
+
 
     public GameManager(RenderWindow renderWindow) {
         this.renderWindow = renderWindow;
     }
 
-    public void initialize() {
 
+    public void initialize() {
+        // Player key listeners
+        new PlayerMousePressListener();
+        new PlayerKeyboardPressListener(myPlayer);
+        new PlayerKeyboardReleaseListener(myPlayer);
     }
 
     public void deinitialize() {
 
     }
-
+    
     public void tick() {
-
+        myPlayer.update(renderWindow);
     }
 
 }
