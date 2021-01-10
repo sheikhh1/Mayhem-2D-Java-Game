3aaf4caba2d9703fc4a4f4d900127d0f274f86d2
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-10 10:50:24 +0000

    Added generic GameManager functions

--- src/me/mayhem/game/GameManager.java
+++ src/me/mayhem/game/GameManager.java
@@ -1,4 +1,25 @@
 package me.mayhem.game;
 
+import org.jsfml.graphics.RenderWindow;
+
 public class GameManager {
+
+    private final RenderWindow renderWindow;
+
+    public GameManager(RenderWindow renderWindow) {
+        this.renderWindow = renderWindow;
+    }
+
+    public void initialize() {
+
+    }
+
+    public void deinitialize() {
+
+    }
+
+    public void tick() {
+
+    }
+
 }
