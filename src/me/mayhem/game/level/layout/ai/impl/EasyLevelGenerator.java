d5410b29adeda54cf3c232e1aacd5eb529c4db85
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-10 13:15:13 +0000

    Changed easy level generator to use builder

--- src/me/mayhem/game/level/layout/ai/impl/EasyLevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/impl/EasyLevelGenerator.java
@@ -1,9 +1,9 @@
 package me.mayhem.game.level.layout.ai.impl;
 
-import me.mayhem.game.collision.impl.RectangleHitbox;
 import me.mayhem.game.level.layout.ai.LevelGenerator;
 import me.mayhem.game.level.layout.block.Block;
 import me.mayhem.math.Vector;
+import org.jsfml.graphics.Color;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.system.Vector2f;
 
@@ -26,6 +26,11 @@ public class EasyLevelGenerator implements LevelGenerator {
         int width = 200;
         int height = 10;
 
-        return new Block(position, new RectangleShape(new Vector2f(width, height)), new RectangleHitbox(position, height, width), color);
+        return Block.builder().color(Color.RED)
+                .drawable(new RectangleShape(new Vector2f(width, height)))
+                .position(position)
+                .width(width)
+                .height(height)
+                .build();
     }
 }
