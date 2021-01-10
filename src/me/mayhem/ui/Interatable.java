9fd2febb96a88c8336bf573d46a48c4f8e752123
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-10 10:34:43 +0000

    Removed onInteract - now unused

--- src/me/mayhem/ui/Interatable.java
+++ src/me/mayhem/ui/Interatable.java
@@ -1,14 +1,9 @@
 package me.mayhem.ui;
 
-import org.jsfml.graphics.RenderWindow;
-import org.jsfml.window.event.Event;
-
 public interface Interatable {
 
     void register();
 
     void unregister();
-
-    void onInteract(RenderWindow window, Event event);
 
 }
