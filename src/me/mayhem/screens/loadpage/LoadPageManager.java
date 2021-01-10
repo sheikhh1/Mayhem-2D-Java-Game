55d8711da2f8057c8a21d62d59d10b3922bf7203
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-10 10:46:06 +0000

    comment abstractHitbox

---
+++ src/me/mayhem/screens/loadpage/LoadPageManager.java
@@ -0,0 +1,4 @@
+package me.mayhem.screens.loadpage;
+
+public class LoadPageManager {
+}
---
+++ src/me/mayhem/screens/newgamesettingspages/NewGameSettingsPageManager.java
@@ -0,0 +1,4 @@
+package me.mayhem.screens.newgamesettingspages;
+
+public class NewGameSettingsPageManager {
+}
