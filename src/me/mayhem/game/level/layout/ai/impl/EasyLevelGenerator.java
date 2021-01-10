9d4da923489229ac799c9bfae23021d28b43456d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-10 13:02:43 +0000

    Created one block

--- src/me/mayhem/game/level/layout/ai/impl/EasyLevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/impl/EasyLevelGenerator.java
@@ -1,14 +1,31 @@
 package me.mayhem.game.level.layout.ai.impl;
 
+import me.mayhem.game.collision.impl.RectangleHitbox;
 import me.mayhem.game.level.layout.ai.LevelGenerator;
 import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.math.Vector;
+import org.jsfml.graphics.RectangleShape;
+import org.jsfml.system.Vector2f;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EasyLevelGenerator implements LevelGenerator {
+
     @Override
     public List<Block> generateLevel() {
-        return Collections.emptyList(); //TODO:
+        List<Block> blocks = new ArrayList<>();
+
+        blocks.add(this.createFloor());
+
+        return blocks;
+    }
+
+    private Block createFloor() {
+        Vector position = new Vector(0, 100);
+        int width = 200;
+        int height = 10;
+
+        return new Block(position, new RectangleShape(new Vector2f(width, height)), new RectangleHitbox(position, height, width));
     }
 }
