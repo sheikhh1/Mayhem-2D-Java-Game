63879c8c261c2799647ef3ef06ee9b15bf904bb3
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-10 11:12:36 +0000

    Added unregister listener method
    - Created Variables for Player Listeners

--- src/me/mayhem/game/GameManager.java
+++ src/me/mayhem/game/GameManager.java
@@ -4,6 +4,7 @@ import me.mayhem.game.entity.player.Player;
 import me.mayhem.game.entity.player.listeners.PlayerKeyboardPressListener;
 import me.mayhem.game.entity.player.listeners.PlayerKeyboardReleaseListener;
 import me.mayhem.game.entity.player.listeners.PlayerMousePressListener;
+import me.mayhem.input.InputManager;
 import me.mayhem.math.Vector;
 import org.jsfml.graphics.RenderWindow;
 
@@ -11,24 +12,39 @@ public class GameManager {
 
     private final RenderWindow renderWindow;
     private Player myPlayer = new Player("test", new Vector(100,100));
+    private PlayerMousePressListener playerMousePress;
+    private PlayerKeyboardPressListener playerKeyPress;
+    private PlayerKeyboardReleaseListener playerKeyRelease;
 
 
     public GameManager(RenderWindow renderWindow) {
         this.renderWindow = renderWindow;
     }
 
-
+    /**
+     * Initalise key & mouse listeners on game start up
+     */
     public void initialize() {
-        // Player key listeners
-        new PlayerMousePressListener();
-        new PlayerKeyboardPressListener(myPlayer);
-        new PlayerKeyboardReleaseListener(myPlayer);
+        playerMousePress = new PlayerMousePressListener();
+        playerKeyPress = new PlayerKeyboardPressListener(myPlayer);
+        playerKeyRelease = new PlayerKeyboardReleaseListener(myPlayer);
+
+
     }
 
+    /**
+     * Remove registered listeners when user leaves game screen
+     */
     public void deinitialize() {
+        InputManager.unregisterInput(playerMousePress);
+        InputManager.unregisterInput(playerKeyPress);
+        InputManager.unregisterInput(playerKeyRelease);
 
     }
-    
+
+    /**
+     * Update drawables
+     */
     public void tick() {
         myPlayer.update(renderWindow);
     }
