4e5799ff324dbcdc1179059fc92d4f0e36d99dfa
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-11 19:58:51 +0000

    Implemented getPlayerSpawnPosition

--- src/me/mayhem/game/level/layout/ai/impl/MediumLevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/impl/MediumLevelGenerator.java
@@ -2,6 +2,7 @@ package me.mayhem.game.level.layout.ai.impl;
 
 import me.mayhem.game.level.layout.ai.LevelGenerator;
 import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.math.Vector;
 
 import java.util.Collections;
 import java.util.List;
@@ -10,5 +11,10 @@ public class MediumLevelGenerator implements LevelGenerator {
     @Override
     public List<Block> generateLevel() {
         return Collections.emptyList(); //TODO:
+    }
+
+    @Override
+    public Vector getPlayerSpawnPosition() {
+        return null;
     }
 }
