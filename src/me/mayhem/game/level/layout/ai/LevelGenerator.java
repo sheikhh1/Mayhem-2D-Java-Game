d8a3137ba8e110dcb66b3f2631971112cc9f8540
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-11 20:02:58 +0000

    Commented

--- src/me/mayhem/game/level/layout/ai/LevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/LevelGenerator.java
@@ -7,8 +7,16 @@ import java.util.List;
 
 public interface LevelGenerator {
 
+    /**
+     * Generates a level dependent on difficulty
+     * @return A list of blocks read from the level designs (.png)
+     */
     List<Block> generateLevel();
 
+    /**
+     * Player spawning position dependent on level
+     * @return Player position as a vector
+     */
     Vector getPlayerSpawnPosition();
 
 }
