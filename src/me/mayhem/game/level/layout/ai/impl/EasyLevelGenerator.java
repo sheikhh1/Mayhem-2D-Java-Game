b3774a1a7fa23275204c185776eac14d363fb2c8
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-11 18:44:16 +0000

    Implementing Level Image Loader

--- src/me/mayhem/game/level/layout/ai/impl/EasyLevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/impl/EasyLevelGenerator.java
@@ -4,30 +4,62 @@ import me.mayhem.game.level.layout.ai.LevelGenerator;
 import me.mayhem.game.level.layout.block.Block;
 import me.mayhem.math.Vector;
 import org.jsfml.graphics.Color;
+import org.jsfml.graphics.Image;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.system.Vector2f;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EasyLevelGenerator implements LevelGenerator {
 
+    private Image levelImage= new Image();
+    private LevelImageLoader loader = new LevelImageLoader();
+    private List<Block> blocks = new ArrayList<>();
+
     @Override
     public List<Block> generateLevel() {
-        List<Block> blocks = new ArrayList<>();
 
-        blocks.add(this.createFloor());
+        levelImage = loader.loadLevel("src/me/mayhem/game/level/layout/leveldesign/test.png");
+
+        loadLevel(levelImg);
 
         return blocks;
     }
 
-    private Block createFloor() {
-        Vector position = new Vector(0, 450);
-        int width = 640;
-        int height = 30;
+    private void loadLevel(Image level) {
+        BufferedImage level1 = level.toBufferedImage();
+
+        int w = level1.getWidth();
+        int h = level1.getHeight();
+
+        System.out.println(w + " " + h);
+
+        for (int x = 0; x < h; x++) {
+            for (int y = 0; y < w; y++) {
+                int pixel = level1.getRGB(x,y);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+
+                if (red == 255 && green == 255 & blue == 255) {
+                    blocks.add(createBlock(x*32,y*32));
+                }
+            }
+        }
+    }
+
+    private Block createBlock(float x, float y) {
+        Vector position = new Vector(x, y);
+        int width = 31;
+        int height = 31;
 
         return Block.builder()
                 .fillColor(Color.RED)
+                .outlineColor(Color.GREEN)
                 .drawable(new RectangleShape(new Vector2f(width, height)))
                 .position(position)
                 .width(width)
