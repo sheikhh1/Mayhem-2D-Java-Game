a4ad33e152be73ee7f8e7f26f1f0d489950f99b7
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-11 19:59:56 +0000

    Added getPlayerSpawn method to Level Generator Interface

--- src/me/mayhem/game/level/layout/ai/LevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/LevelGenerator.java
@@ -1,11 +1,14 @@
 package me.mayhem.game.level.layout.ai;
 
 import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.math.Vector;
 
 import java.util.List;
 
 public interface LevelGenerator {
 
     List<Block> generateLevel();
+
+    Vector getPlayerSpawnPosition();
 
 }
