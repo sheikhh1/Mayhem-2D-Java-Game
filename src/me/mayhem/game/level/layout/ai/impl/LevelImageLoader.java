71c18ea70aac2db0516d174cc10d7736e12fd924
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-11 18:48:24 +0000

    Refactored

--- src/me/mayhem/game/level/layout/ai/impl/LevelImageLoader.java
+++ src/me/mayhem/game/level/layout/ai/impl/LevelImageLoader.java
@@ -3,16 +3,17 @@ package me.mayhem.game.level.layout.ai.impl;
 import org.jsfml.graphics.Image;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class LevelImageLoader {
 
     private Image testLevel;
 
-    public Image loadLevel(String path) {
+    public Image loadLevel(Path path) {
 
         try {
-            testLevel.loadFromFile(Paths.get(path));
+            testLevel.loadFromFile(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
