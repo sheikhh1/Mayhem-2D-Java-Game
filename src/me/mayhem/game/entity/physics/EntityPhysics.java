203f1b8c7520412011432473cfeb1bfdfe423ed9
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-12 15:36:34 +0000

    Refactored

--- src/me/mayhem/game/entity/physics/EntityPhysics.java
+++ src/me/mayhem/game/entity/physics/EntityPhysics.java
@@ -1,43 +1,43 @@
-package me.mayhem.game.entity.player.physics;
+package me.mayhem.game.entity.physics;
 
 import me.mayhem.util.Vector;
 
-public class PlayerPhysics {
+public class EntityPhysics {
 
     private static final float GRAVITY = 1.5f;
     private static final float MAX_SPEED = 5f;
 
     private float jumpStrength = 20f;
     private float fallStrength = 0;
-    private Vector playerMotion;
+    private Vector motion;
 
     /**
      * Set Player motion
      * @param motion - Motion passed by player class
      */
-    public void setPlayerMotion(Vector motion) {
-        this.playerMotion = motion;
+    public void setEntityMotion(Vector motion) {
+        this.motion = motion;
     }
 
     /**
      * Move Player Forward method by 5 Pixels
      */
     public void moveForward() {
-        this.playerMotion.add(MAX_SPEED,0);
+        this.motion.add(MAX_SPEED,0);
     }
 
     /**
      * Move Player Back method by 5 Pixels
      */
     public void moveBack() {
-        this.playerMotion.subtract(MAX_SPEED,0);
+        this.motion.subtract(MAX_SPEED,0);
     }
 
     /**
      * Jump method for the Player
      */
     public void jump() {
-        this.playerMotion.subtract(0, this.jumpStrength);
+        this.motion.subtract(0, this.jumpStrength);
         this.jumpStrength -= GRAVITY;
     }
 
@@ -45,7 +45,7 @@ public class PlayerPhysics {
      * Basic Fall Method Added
      */
     public void fall() {
-        this.playerMotion.add(0, this.fallStrength);
+        this.motion.add(0, this.fallStrength);
         this.fallStrength += GRAVITY;
     }
 }
