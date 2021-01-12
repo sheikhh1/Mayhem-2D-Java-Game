b04ff4c1a37ba16f665537b7932af86432aaca0c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-12 10:19:05 +0000

    Added Layout#moveBlocks

--- src/me/mayhem/game/level/layout/Layout.java
+++ src/me/mayhem/game/level/layout/Layout.java
@@ -2,6 +2,7 @@ package me.mayhem.game.level.layout;
 
 import me.mayhem.game.level.difficulty.Difficulty;
 import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.util.Vector;
 import org.jsfml.graphics.RenderWindow;
 
 import java.util.ArrayList;
@@ -18,6 +19,12 @@ public class Layout {
     public void draw(RenderWindow renderWindow) {
         for (Block block : this.blocks) {
             renderWindow.draw(block.getDrawable());
+        }
+    }
+
+    public void moveBlocks(Vector direction) {
+        for (Block block : this.blocks) {
+            block.getPosition().add(direction);
         }
     }
 }
