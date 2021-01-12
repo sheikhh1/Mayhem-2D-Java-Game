20fd9a304d3d7487b7a4b93a9e1ede330bdaa36d
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-12 13:31:44 +0000

    All buttons that dont start a game are working

--- src/me/mayhem/screens/loadpage/items/LoadPageReturnButton.java
+++ src/me/mayhem/screens/loadpage/items/LoadPageReturnButton.java
@@ -1,5 +1,8 @@
 package me.mayhem.screens.loadpage.items;
 
+import me.mayhem.Mayhem;
+import me.mayhem.screens.homepage.HomePageManager;
+import me.mayhem.screens.loadpage.LoadPageManager;
 import me.mayhem.ui.AbstractInteractable;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
@@ -12,6 +15,9 @@ public class LoadPageReturnButton extends AbstractInteractable {
 
     @Override
     protected void call(RenderWindow window, Event event) {
-
+        if ( event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
+            Mayhem.getCurrentScreen().unloadScreen(window);
+            Mayhem.setCurrentScreen(new HomePageManager(window));
+        }
     }
 }
