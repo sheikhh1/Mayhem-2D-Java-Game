defd6e7428bb57cbd187f0e3dfcbc0c8f22b5911
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-12 14:07:20 +0000

    Added font to button constructor

--- src/me/mayhem/ui/impl/ButtonInteractable.java
+++ src/me/mayhem/ui/impl/ButtonInteractable.java
@@ -1,14 +1,16 @@
 package me.mayhem.ui.impl;
 
 import me.mayhem.ui.AbstractInteractable;
+import org.jsfml.graphics.Font;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
+import org.jsfml.graphics.Text;
 
 public abstract class ButtonInteractable extends AbstractInteractable {
 
     private String[] text;
 
-    public ButtonInteractable(Shape shape, String... text) {
+    public ButtonInteractable(Shape shape, Font font, String... text) {
         super(shape);
 
         this.text = text;
@@ -16,6 +18,9 @@ public abstract class ButtonInteractable extends AbstractInteractable {
 
     @Override
     public void draw(RenderWindow renderWindow) {
+        Text text = new Text("Test", );
+
+        renderWindow.draw(super.shape);
 
     }
 }
