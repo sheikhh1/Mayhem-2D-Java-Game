36cdee7ce2157477195b9f95ac1032635ec613d7
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-12 17:20:04 +0000

    Revert "Added testing Game Screen"
    This reverts commit 3c7574616297986485f8d38991bcbf5a51772ce3.

--- src/me/mayhem/screens/newgamesettingspage/items/SettingsPageEasyButton.java
+++ src/me/mayhem/screens/newgamesettingspage/items/SettingsPageEasyButton.java
@@ -1,8 +1,5 @@
 package me.mayhem.screens.newgamesettingspage.items;
 
-import me.mayhem.Mayhem;
-import me.mayhem.screens.gamescreen.GameScreenManager;
-import me.mayhem.screens.newgamesettingspage.NewGameSettingsPageManager;
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
@@ -15,9 +12,6 @@ public class SettingsPageEasyButton extends ButtonInteractable {
 
     @Override
     protected void call(RenderWindow window, Event event) {
-        if (event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
-            Mayhem.getCurrentScreen().unloadScreen(window);
-            Mayhem.setCurrentScreen(new GameScreenManager(window));
-        }
+
     }
 }
