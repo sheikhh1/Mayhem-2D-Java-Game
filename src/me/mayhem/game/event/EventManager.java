691231eccadb9bc32831f677f7013051caa378e1
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 12:39:56 +0000

    Added EventListener inner class

--- src/me/mayhem/game/event/EventManager.java
+++ src/me/mayhem/game/event/EventManager.java
@@ -1,4 +1,39 @@
 package me.mayhem.game.event;
 
+import java.util.Map;
+import java.util.function.Consumer;
+
 public class EventManager {
+
+    private static Map<Class<? extends Event>, >
+
+    public static void registerListener(Class<?> listenerClazz) {
+
+    }
+
+    private static final class EventListener {
+
+        private final Consumer<Event> consumer;
+        private final EventPriority priority;
+        private final boolean ignoreCancelled;
+
+        public EventListener(Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
+            this.consumer = consumer;
+            this.priority = priority;
+            this.ignoreCancelled = ignoreCancelled;
+        }
+
+        public Consumer<Event> getConsumer() {
+            return this.consumer;
+        }
+
+        public EventPriority getPriority() {
+            return this.priority;
+        }
+
+        public boolean isIgnoreCancelled() {
+            return this.ignoreCancelled;
+        }
+    }
+
 }
