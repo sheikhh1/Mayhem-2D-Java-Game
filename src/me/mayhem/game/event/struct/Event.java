c5103b1306bcc8c02f34eb08954a31fec5262303
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 13:25:22 +0000

    Commented Event class

--- src/me/mayhem/game/event/struct/Event.java
+++ src/me/mayhem/game/event/struct/Event.java
@@ -1,13 +1,30 @@
 package me.mayhem.game.event.struct;
 
+/**
+ *
+ * Base class for all events
+ *
+ */
 public abstract class Event {
 
     private boolean cancelled = false;
 
+    /**
+     *
+     * Check if the event has been cancelled
+     *
+     * @return if the event has been cancelled this will be true
+     */
     public boolean isCancelled() {
         return this.cancelled;
     }
 
+    /**
+     *
+     * Cancel or uncancel the event
+     *
+     * @param cancelled Sets if the event has been cancelled
+     */
     public void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
     }
