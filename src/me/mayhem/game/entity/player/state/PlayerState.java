99e0a0cd46f4a57a06d04dc73b27cc2f662286eb
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 11:07:18 +0000

    Added playerstate index

--- src/me/mayhem/game/entity/player/state/PlayerState.java
+++ src/me/mayhem/game/entity/player/state/PlayerState.java
@@ -2,12 +2,21 @@ package me.mayhem.game.entity.player.state;
 
 public enum PlayerState {
 
-    FORWARD(),
-    BACK(),
-    JUMPING(),
-    FALLING(),
-    STANDING(),
+    FORWARD(1),
+    BACK(1),
+    STANDING(1),
+    JUMPING(0),
+    FALLING(0),
 
     ;
 
+    private int index;
+
+    PlayerState(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
 }
