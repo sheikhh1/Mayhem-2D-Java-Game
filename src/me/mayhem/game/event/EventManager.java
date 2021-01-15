d74d35e3c1f1ba06c5d303c51e870e304cd30bcc
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 12:54:25 +0000

    Added EventManager#unregisterListeners

--- src/me/mayhem/game/event/EventManager.java
+++ src/me/mayhem/game/event/EventManager.java
@@ -1,33 +1,92 @@
 package me.mayhem.game.event;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class EventManager {
 
-    private static final Map<Class<? extends Event>, List<EventListener>> REGISTERED_LISTENERS = new HashMap<>();
+    private static final Map<Class<? extends Event>, Queue<EventHandler>> REGISTERED_LISTENERS = new HashMap<>();
+    private static final Comparator<EventHandler> HANDLER_COMPARATOR = Comparator.comparingInt(o -> o.getPriority().ordinal());
 
-    public static void registerListener(Class<?> listenerClazz) {
-        for (Method declaredMethod : listenerClazz.getDeclaredMethods()) {
+    public static void registerListener(Object object) {
+        for (Method declaredMethod : object.getClass().getDeclaredMethods()) {
             EventListener eventListener = declaredMethod.getAnnotation(EventListener.class);
 
+            if (eventListener == null) {
+                continue;
+            }
+
+            declaredMethod.setAccessible(true);
+
+            if (declaredMethod.getParameterTypes().length != 1) {
+                throw new IllegalArgumentException("EventListener can only have one parameter");
+            }
+
+            if (!(Event.class.isAssignableFrom(declaredMethod.getParameterTypes()[0]))) {
+                throw new IllegalArgumentException("First parameter of EventListener must extend event");
+            }
+
+            Class<? extends Event> eventClass = (Class<? extends Event>) declaredMethod.getParameterTypes()[0];
+
+            EventHandler eventHandler = new EventHandler(object.getClass(), event -> {
+                try {
+                    declaredMethod.invoke(object, event);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }, eventListener.priority(), eventListener.ignoreCancelled());
+
+            REGISTERED_LISTENERS.computeIfAbsent(eventClass, ___ -> new PriorityQueue<>(HANDLER_COMPARATOR)).add(eventHandler);
         }
+    }
+
+    public static void unregisterListeners(Object object) {
+        for (Method declaredMethod : object.getClass().getDeclaredMethods()) {
+            EventListener eventListener = declaredMethod.getAnnotation(EventListener.class);
+
+            if (eventListener == null) {
+                continue;
+            }
+
+            declaredMethod.setAccessible(true);
+
+            if (declaredMethod.getParameterTypes().length != 1) {
+                continue;
+            }
+
+            if (!(Event.class.isAssignableFrom(declaredMethod.getParameterTypes()[0]))) {
+                continue;
+            }
+
+            Class<? extends Event> eventClass = (Class<? extends Event>) declaredMethod.getParameterTypes()[0];
+
+            REGISTERED_LISTENERS.getOrDefault(eventClass, new PriorityQueue<>(HANDLER_COMPARATOR))
+                    .removeIf(handler -> handler.getParentClass() == object.getClass());
+        }
+    }
+
+    public static void callEvent() {
 
     }
 
     private static final class EventHandler {
 
+        private final Class<?> parentClass;
         private final Consumer<Event> consumer;
         private final EventPriority priority;
         private final boolean ignoreCancelled;
 
-        public EventHandler(Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
+        public EventHandler(Class<?> parentClass, Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
+            this.parentClass = parentClass;
             this.consumer = consumer;
             this.priority = priority;
             this.ignoreCancelled = ignoreCancelled;
+        }
+
+        public Class<?> getParentClass() {
+            return this.parentClass;
         }
 
         public Consumer<Event> getConsumer() {
