1b9d1d3e6d84c581d8ec7665fadaa3c809ea4a7a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 13:03:18 +0000

    Added EventHandler.Builder

--- src/me/mayhem/game/event/struct/EventHandler.java
+++ src/me/mayhem/game/event/struct/EventHandler.java
@@ -9,7 +9,7 @@ public class EventHandler {
     private final EventPriority priority;
     private final boolean ignoreCancelled;
 
-    public EventHandler(Class<?> parentClass, Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
+    private EventHandler(Class<?> parentClass, Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
         this.parentClass = parentClass;
         this.consumer = consumer;
         this.priority = priority;
@@ -32,6 +32,37 @@ public class EventHandler {
         return this.ignoreCancelled;
     }
 
+    public static Builder builder() {
+        return new Builder();
+    }
 
+    public static final class Builder {
 
+        private Class<?> parentClass;
+        private Consumer<Event> consumer;
+        private EventPriority eventPriority;
+        private boolean ignoreCancelled;
+
+        protected Builder() {}
+
+        public Builder parentClass(Class<?> parentClass) {
+            this.parentClass = parentClass;
+            return this;
+        }
+
+        public Builder consumer(Consumer<Event> consumer) {
+            this.consumer = consumer;
+            return this;
+        }
+
+        public Builder eventListener(EventListener eventListener) {
+            this.eventPriority = eventListener.priority();
+            this.ignoreCancelled = eventListener.ignoreCancelled();
+            return this;
+        }
+
+        public EventHandler build() {
+            return new EventHandler(this.parentClass, this.consumer, this.eventPriority, this.ignoreCancelled);
+        }
+    }
 }
