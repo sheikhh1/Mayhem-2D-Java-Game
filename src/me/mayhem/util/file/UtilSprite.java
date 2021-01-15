de159562ae44a97be4c357bf1aee67438d5897ba
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 16:39:45 +0000

    Created UtilSprite

---
+++ src/me/mayhem/util/file/UtilSprite.java
@@ -0,0 +1,23 @@
+package me.mayhem.util.file;
+
+import me.mayhem.Mayhem;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
+
+import java.io.IOException;
+
+public class UtilSprite {
+
+    public static Sprite spriteFromPath(String path) {
+        Texture newTexture = new Texture();
+
+        try {
+            newTexture.loadFromStream(Mayhem.class.getClassLoader().getResourceAsStream(path));
+            return new Sprite(newTexture);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
