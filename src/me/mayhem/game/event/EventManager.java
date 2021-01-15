a4b5bacd36b0dd67e2d785bdbba28f49e7ea5057
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 13:23:30 +0000

    Commented EventManager

--- src/me/mayhem/game/event/EventManager.java
+++ src/me/mayhem/game/event/EventManager.java
@@ -8,11 +8,23 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 
+/**
+ *
+ * EventManager static factory for registering, unregistering and calling game events.
+ *
+ */
 public class EventManager {
 
     private static final Map<Class<? extends Event>, Queue<EventHandler>> REGISTERED_LISTENERS = new HashMap<>();
     private static final Comparator<EventHandler> HANDLER_COMPARATOR = Comparator.comparingInt(o -> o.getPriority().ordinal());
 
+    /**
+     *
+     * Iterates through all declared methods in the class and finds those annotated with {@link EventListener} and
+     * caches them to be called when the Event occurs.
+     *
+     * @param object The class with {@link EventListener} annotated methods to be registered
+     */
     public static void registerListener(Object object) {
         for (Method declaredMethod : object.getClass().getDeclaredMethods()) {
             EventListener eventListener = declaredMethod.getAnnotation(me.mayhem.game.event.struct.EventListener.class);
@@ -42,6 +54,14 @@ public class EventManager {
         }
     }
 
+    /**
+     *
+     * Invokes the method from the class witht he event object
+     *
+     * @param event The event occurring
+     * @param object The object which the method belongs to
+     * @param method The method being called when the event occurs
+     */
     private static void invokeEvent(Event event, Object object, Method method) {
         try {
             method.invoke(object, event);
@@ -50,6 +70,13 @@ public class EventManager {
         }
     }
 
+    /**
+     *
+     * Iterates through all declared methods in the class and finds those annotated with {@link EventListener} and
+     * unregisters all the methods.
+     *
+     * @param object The class with {@link EventListener} annotated methods to be unregistered
+     */
     public static void unregisterListeners(Object object) {
         for (Method declaredMethod : object.getClass().getDeclaredMethods()) {
             EventListener eventListener = declaredMethod.getAnnotation(EventListener.class);
@@ -71,6 +98,12 @@ public class EventManager {
         }
     }
 
+    /**
+     *
+     * Calls the event to all registered event listeners
+     *
+     * @param event The event being called
+     */
     public static void callEvent(Event event) {
         for (EventHandler eventHandler : REGISTERED_LISTENERS.get(event.getClass())) {
             eventHandler.getConsumer().accept(event);
