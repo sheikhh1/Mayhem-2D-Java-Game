73c2815cd77d1a1022acbb7239703762cf7d5b2d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 10:36:11 +0000

    Added Vector#clone

--- src/me/mayhem/util/Vector.java
+++ src/me/mayhem/util/Vector.java
@@ -97,4 +97,9 @@ public class Vector {
                 ", y=" + this.getY() +
                 '}';
     }
+
+    @Override
+    public Vector clone() {
+        return new Vector(this.getX(), this.getY());
+    }
 }
