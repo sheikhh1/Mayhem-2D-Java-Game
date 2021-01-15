bdd7e356210ff4a2b038550e417b7b299e4d67e9
vsood1 <soodv2@lancaster.ac.uk>
2021-01-15 11:31:10 +0000

    added files

--- src/me/mayhem/game/level/layout/ai/audio/Audio.java
+++ src/me/mayhem/game/level/layout/ai/audio/Audio.java
@@ -1,8 +1,21 @@
 package me.mayhem.game.level.layout.ai.audio;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 /**
  * Audio Class
  * Generates methods to load the audio files
  */
 
 public class Audio {
+    try {
+        soundBuffer.loadFromFile(Paths.get("sound.wav"));
+        System.out.println("Sound duration: " + soundBuffer.getDuration().asSeconds() + " seconds");
+    } catch(
+    IOException ex) {
+        //Something went wrong
+        System.err.println("Failed to load the sound:");
+        ex.printStackTrace();
+    }
 }
