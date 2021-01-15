5d4e7f806e2f6e7f30007c0a9ed9b987c3ea0cda
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-15 12:43:17 +0000

    Refactored to EventHandler

--- src/me/mayhem/game/event/EventManager.java
+++ src/me/mayhem/game/event/EventManager.java
@@ -1,23 +1,30 @@
 package me.mayhem.game.event;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
 public class EventManager {
 
-    private static Map<Class<? extends Event>, >
+    private static final Map<Class<? extends Event>, List<EventListener>> REGISTERED_LISTENERS = new HashMap<>();
 
     public static void registerListener(Class<?> listenerClazz) {
+        for (Method declaredMethod : listenerClazz.getDeclaredMethods()) {
+            EventListener eventListener = declaredMethod.getAnnotation(EventListener.class);
+
+        }
 
     }
 
-    private static final class EventListener {
+    private static final class EventHandler {
 
         private final Consumer<Event> consumer;
         private final EventPriority priority;
         private final boolean ignoreCancelled;
 
-        public EventListener(Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
+        public EventHandler(Consumer<Event> consumer, EventPriority priority, boolean ignoreCancelled) {
             this.consumer = consumer;
             this.priority = priority;
             this.ignoreCancelled = ignoreCancelled;
