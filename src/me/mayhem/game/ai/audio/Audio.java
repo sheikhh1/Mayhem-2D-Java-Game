56417b96f1d88c8a0ed447f374bc2e8669bd9db9
vsood1 <soodv2@lancaster.ac.uk>
2021-01-19 13:53:32 +0000

    created new completedgame method

--- src/me/mayhem/game/ai/audio/Audio.java
+++ src/me/mayhem/game/ai/audio/Audio.java
@@ -1,5 +1,8 @@
 package me.mayhem.game.ai.audio;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Audio {
 
 /**
@@ -7,16 +10,18 @@ public class Audio {
  * Generates methods to load the audio files
  */
 
-//public class Audio {
-//    try {
-//        soundBuffer.loadFromFile(Paths.get("sound.wav"));
-//        System.out.println("Sound duration: " + soundBuffer.getDuration().asSeconds() + " seconds");
-//    } catch(
-//    IOException ex) {
-//        //Something went wrong
-//        System.err.println("Failed to load the sound:");
-//        ex.printStackTrace();
-//    }
-//}
+public class Audio {
+
+    public void completedgame(){
+        try {
+            soundBuffer.loadFromFile(Paths.get("sound.wav"));
+            System.out.println("Sound duration: " + soundBuffer.getDuration().asSeconds() + " seconds");
+        } catch(
+                IOException ex) {
+            //Something went wrong
+            System.err.println("Failed to load the sound:");
+            ex.printStackTrace();
+        }
+    }
 
 }
