6a8ca53028ac856c0c9dfbabbc983a2a04ce7e35
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-20 14:06:34 +0000

    Implemented EasilyEntitySpawner

--- src/me/mayhem/game/level/spawning/impl/EasyEntitySpawner.java
+++ src/me/mayhem/game/level/spawning/impl/EasyEntitySpawner.java
@@ -1,6 +1,6 @@
 package me.mayhem.game.level.spawning.impl;
 
-import me.mayhem.game.entity.enemies.infected.Infected;
+import me.mayhem.game.entity.enemies.infected.InfectedEnemy;
 import me.mayhem.game.level.Level;
 import me.mayhem.game.level.spawning.EntitySpawner;
 import me.mayhem.util.Vector;
@@ -11,7 +11,10 @@ public class EasyEntitySpawner implements EntitySpawner {
     public void spawnEntities(Level level) {
         //TODO:
         // create something that works based on the constraints of the editor
-        level.getEntities().add(new Infected(new Vector(500,500)));
+
+        for (int i = 0; i < level.getDifficulty().getGenerator().getEnemySpawnPosition().size(); i++) {
+            level.spawnEnemy(new InfectedEnemy(level.getDifficulty().getGenerator().getEnemySpawnPosition().get(i)));
+        }
 
     }
 }
