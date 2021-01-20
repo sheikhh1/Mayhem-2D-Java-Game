e2c9d56151d0ae7fdee929d434b1671b3b548bb4
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-20 12:51:59 +0000

    added the sprite and button arrays

--- src/me/mayhem/screens/escapescreen/EscapeScreenManager.java
+++ src/me/mayhem/screens/escapescreen/EscapeScreenManager.java
@@ -1,11 +1,18 @@
 package me.mayhem.screens.escapescreen;
 
+import me.mayhem.Mayhem;
 import me.mayhem.game.level.difficulty.Difficulty;
 import me.mayhem.screens.ScreenManager;
+import me.mayhem.util.UtilSharedResources;
+import me.mayhem.util.Vector;
+import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.audio.Sound;
-import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.*;
 
 public class EscapeScreenManager implements ScreenManager {
+    private Sprite[] sprites;
+    private ButtonInteractable[] buttons;
+
     @Override
     public void loadScreen(RenderWindow renderWindow) {
 
@@ -34,5 +41,24 @@ public class EscapeScreenManager implements ScreenManager {
     @Override
     public Sound getSound() {
         return null;
+    }
+
+    private void createSprites() {
+        Sprite background = UtilSharedResources.getBackground();
+
+        this.sprites = new Sprite[]{background};
+    }
+    private void createButtons(){
+
+    }
+
+    private Shape createReturnButton() {
+        RectangleShape shape = new RectangleShape();
+
+        shape.setSize(new Vector(200, 100).toVector());
+        shape.setPosition((0), (Mayhem.SCREEN_HEIGHT - 100));
+        shape.setFillColor(new Color(176, 176, 176));
+
+        return shape;
     }
 }
