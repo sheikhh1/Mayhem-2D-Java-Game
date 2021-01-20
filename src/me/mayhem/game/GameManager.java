bd5a7ce25a998e73f5fd415aae547d72da03bab8
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-20 22:01:49 +0000

    Set collided attribute in entity

--- src/me/mayhem/game/GameManager.java
+++ src/me/mayhem/game/GameManager.java
@@ -168,6 +168,7 @@ public class GameManager {
                         }
 
                         collisionDetectedX = true;
+                        entity.setAttribute("collided", true);
                     }
 
                     if (this.isLowerThenEntity(entity, block) && !collisionDetected) {
