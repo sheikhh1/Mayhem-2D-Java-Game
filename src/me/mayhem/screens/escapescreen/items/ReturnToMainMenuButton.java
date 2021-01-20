61d5f011c890d360c0b2fbc13f209d34731a3374
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-20 13:24:56 +0000

    extending button interable

--- src/me/mayhem/screens/escapescreen/items/ReturnToMainMenuButton.java
+++ src/me/mayhem/screens/escapescreen/items/ReturnToMainMenuButton.java
@@ -1,4 +1,13 @@
 package me.mayhem.screens.escapescreen.items;
 
-public class ReturnToMainMenuButton {
+import me.mayhem.util.ui.impl.ButtonInteractable;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.event.Event;
+
+public class ReturnToMainMenuButton extends ButtonInteractable {
+
+    @Override
+    protected void call(RenderWindow window, Event event) {
+
+    }
 }
