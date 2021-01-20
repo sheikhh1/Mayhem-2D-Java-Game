dec70aafc239d49610b9b258748c82ead3e1b214
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-20 15:14:56 +0000

    Implemented JumpSound as listener

--- src/me/mayhem/game/ai/audio/impl/JumpSound.java
+++ src/me/mayhem/game/ai/audio/impl/JumpSound.java
@@ -1,17 +1,30 @@
 package me.mayhem.game.ai.audio.impl;
 
 import me.mayhem.game.ai.audio.AbstractAudioPlayer;
+import me.mayhem.game.entity.player.event.PlayerJumpEvent;
+import me.mayhem.game.event.struct.EventListener;
 import org.jsfml.audio.Sound;
 
 public class JumpSound extends AbstractAudioPlayer {
+
+    private static final String JUMP_SOUND_PATH = "audio/jump.wav";
+
     private Sound sound;
+
+    public JumpSound() {
+        super();
+
+        this.sound = this.load(JUMP_SOUND_PATH);
+    }
+
+    @EventListener
+    public void onPlayerJump(PlayerJumpEvent event) {
+        this.play();
+    }
 
     @Override
     public void play() {
-        if (sound == null) {
-            this.sound = this.load("audio/jump.wav");
-        }
-        sound.play();
+        this.sound.play();
     }
 }
 
