2557d281e6797c551b24a39f2e6c75840897cf94
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-20 13:30:39 +0000

    Added AbstractKeyboardMouseInteractable.java

---
+++ src/me/mayhem/util/ui/AbstractKeyboardMouseInteractable.java
@@ -0,0 +1,57 @@
+package me.mayhem.util.ui;
+
+import me.mayhem.input.InputListener;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Shape;
+import org.jsfml.window.event.Event;
+import org.jsfml.window.event.KeyEvent;
+import org.jsfml.window.event.MouseEvent;
+
+/**
+ *
+ * Skeletal implementation of the {@link Interatable} interface
+ * Handles checking for if the {@link MouseEvent} occurs in the correct location
+ *
+ */
+public abstract class AbstractKeyboardMouseInteractable extends InputListener<KeyEvent> implements Interatable {
+
+    protected Shape shape;
+    private boolean focussed;
+
+    public AbstractKeyboardMouseInteractable(Shape shape) {
+        super(Event.Type.KEY_PRESSED, Event.Type.MOUSE_LEFT);
+
+        this.shape = shape;
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        if (!super.getTypes().contains(event.type)) {
+            return;
+        }
+
+        if (event instanceof MouseEvent) {
+            MouseEvent mouseEvent = event.asMouseEvent();
+
+            if (shape.getGlobalBounds().contains(mouseEvent.position.x, mouseEvent.position.y)) {
+                this.focussed = true;
+            } else {
+                this.focussed = false;
+            }
+        }
+
+        if (this.focussed && (event instanceof KeyEvent)) {
+            this.takeInput(event.asKeyEvent());
+        }
+    }
+
+    @Override
+    protected void takeInput(KeyEvent event) {
+
+    }
+
+    @Override
+    public void draw(RenderWindow renderWindow) {
+        renderWindow.draw(this.shape);
+    }
+}
