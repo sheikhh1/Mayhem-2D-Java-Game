9aacaca68165a5e7cb898a37c2258a5085d7a383
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-20 16:15:08 +0000

    Added determineState method
    - Need to sort Y directional functionality

--- src/me/mayhem/game/ai/path/impl/MoveToPlayerPathing.java
+++ src/me/mayhem/game/ai/path/impl/MoveToPlayerPathing.java
@@ -2,12 +2,15 @@ package me.mayhem.game.ai.path.impl;
 
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
 
 public class MoveToPlayerPathing implements Pathing {
 
     private Level currentLevel;
+    private Vector toPlayer;
+    private Entity entity;
 
     public MoveToPlayerPathing(Level currentLevel) {
         this.currentLevel = currentLevel;
@@ -15,9 +18,23 @@ public class MoveToPlayerPathing implements Pathing {
 
     @Override
     public void updatePosition(Entity entity) {
-        Vector toPlayer = currentLevel.getPlayer().getPosition().clone().subtract(entity.getPosition()).normalize();
-
+        this.entity = entity;
+        toPlayer = currentLevel.getPlayer().getPosition().clone().subtract(entity.getPosition()).normalize();
         toPlayer.setY(0);
         entity.getMotion().add(toPlayer);
+
+        this.determineState();
     }
+
+    private void determineState() {
+        if (toPlayer.getX() < 0 ) {
+            this.entity.setState(EntityState.BACK);
+        } else if (toPlayer.getX() > 0){
+            this.entity.setState(EntityState.FORWARD);
+        } else if (toPlayer.getX() == 0){
+            this.entity.setState(EntityState.STANDING);
+        }
+    }
+
+
 }
