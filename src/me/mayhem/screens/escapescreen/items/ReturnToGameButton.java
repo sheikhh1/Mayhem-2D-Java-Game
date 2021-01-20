251f2de99c94a0e211416e4400b02d800ee708e9
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-20 13:26:51 +0000

    Created constructor

--- src/me/mayhem/screens/escapescreen/items/ReturnToGameButton.java
+++ src/me/mayhem/screens/escapescreen/items/ReturnToGameButton.java
@@ -2,11 +2,15 @@ package me.mayhem.screens.escapescreen.items;
 
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 import java.awt.*;
 
 public class ReturnToGameButton extends ButtonInteractable {
+    public ReturnToGameButton(Shape shape) {
+        super(shape, "fonts/FreeSans.ttf", "Return to game ");
+    }
     @Override
     protected void call(RenderWindow window, Event event) {
 
