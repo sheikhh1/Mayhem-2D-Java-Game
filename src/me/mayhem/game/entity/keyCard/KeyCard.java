c3d3821859c83554768a4af08ec6f42e3a8f4f8a
vsood1 <soodv2@lancaster.ac.uk>
2021-01-20 12:07:31 +0000

    commiting to branch with errors to switch to new branch, implemented drawable methods and attempting to load the keycard into game

--- src/me/mayhem/game/entity/keyCard/KeyCard.java
+++ src/me/mayhem/game/entity/keyCard/KeyCard.java
@@ -1,5 +1,28 @@
 package me.mayhem.game.entity.keyCard;
 
-public class KeyCard {
+import me.mayhem.Mayhem;
+import me.mayhem.game.entity.drawable.Drawable;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
 
+import java.io.IOException;
+
+public class KeyCard implements Drawable {
+
+    @Override
+    public void draw(RenderWindow canvas) {
+
+    }
+    public static keyCard loadFromPath("keyCard.png") {
+        Texture newTexture = new Texture();
+
+        try {
+            newTexture.loadFromStream(Mayhem.class.getClassLoader().getResourceAsStream(path));
+            return new KeyCard(newTexture);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
 }
