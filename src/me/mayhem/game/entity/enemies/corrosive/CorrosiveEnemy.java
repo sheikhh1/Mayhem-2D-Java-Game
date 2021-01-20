866963f98249cb562f66f5ecaef4d2ebe8d312b2
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-20 14:57:08 +0000

    Refactored

--- src/me/mayhem/game/entity/enemies/corrosive/CorrosiveEnemy.java
+++ src/me/mayhem/game/entity/enemies/corrosive/CorrosiveEnemy.java
@@ -4,11 +4,9 @@ import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
-import me.mayhem.game.entity.enemies.infected.state.InfectedState;
 import me.mayhem.util.Vector;
 
 public class CorrosiveEnemy extends Entity {
-    private InfectedState[] states = new InfectedState[2];
     /**
      * Entity Constructor
      *
@@ -17,10 +15,6 @@ public class CorrosiveEnemy extends Entity {
     public CorrosiveEnemy(Vector position) {
         super(EntityType.INFECTED, position, Vector.getZero(), new SpriteHitbox(position, 0,0 ), Pathing.FORWARD_PATHING);
 
-    }
-
-    public InfectedState getInfectedState(int index){
-        return this.states[index];
     }
 
     @Override
