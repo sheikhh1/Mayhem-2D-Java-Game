8419cdf6a9345541f1e5ec4ea8783c56e065b592
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-20 12:30:17 +0000

    Added an EscapeScreenManager class

---
+++ src/me/mayhem/screens/escapescreen/EscapeScreenManager.java
@@ -0,0 +1,38 @@
+package me.mayhem.screens.escapescreen;
+
+import me.mayhem.game.level.difficulty.Difficulty;
+import me.mayhem.screens.ScreenManager;
+import org.jsfml.audio.Sound;
+import org.jsfml.graphics.RenderWindow;
+
+public class EscapeScreenManager implements ScreenManager {
+    @Override
+    public void loadScreen(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void unloadScreen(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void draw(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void close(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public Difficulty getDifficulty() {
+        return null;
+    }
+
+    @Override
+    public Sound getSound() {
+        return null;
+    }
+}
