7c4396918a75043ee5083d920565cd7806612e62
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-20 13:26:55 +0000

    Created constructor

--- src/me/mayhem/screens/escapescreen/items/ReturnToMainMenuButton.java
+++ src/me/mayhem/screens/escapescreen/items/ReturnToMainMenuButton.java
@@ -2,9 +2,14 @@ package me.mayhem.screens.escapescreen.items;
 
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 public class ReturnToMainMenuButton extends ButtonInteractable {
+
+    public ReturnToMainMenuButton(Shape shape) {
+        super(shape, "fonts/FreeSans.ttf", "Main Menu ");
+    }
 
     @Override
     protected void call(RenderWindow window, Event event) {
