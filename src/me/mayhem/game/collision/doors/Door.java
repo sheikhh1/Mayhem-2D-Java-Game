e61aaa76384b7d030b64b80f34fc9139a3f405a3
Vishal <v.sood2@lancaster.ac.uk>
2021-01-26 21:53:24 +0000

    switching laptop but need to commit changes

--- src/me/mayhem/game/collision/doors/Door.java
+++ src/me/mayhem/game/collision/doors/Door.java
@@ -7,13 +7,21 @@ import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
 import me.mayhem.util.Vector;
+import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Sprite;
+
+import static java.awt.SystemColor.window;
 
 public class Door extends Entity {
     private Sprite sprite;
 
-    public Projectiles(EntityType type, Vector position, Vector motion, String image, Hitbox hitbox, Pathing pathing, Attribute<?>... attributes) {
+    public Door(EntityType type, Vector position, Vector motion, String image, Hitbox hitbox, Pathing pathing, Attribute<?>... attributes) {
         super(EntityType.DOOR, position, Vector.ZERO, new SpriteHitbox(position, 0, 0), Pathing.NO_PATHING, attributes);
 
         this.sprite = this.loadFromPath(image);
+
+        @Override
+        public void update(RenderWindow window) {
+            window.draw(this.sprite);
+        }
 }
