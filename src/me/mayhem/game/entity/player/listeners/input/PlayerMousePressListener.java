f228947fead8921aa05b45e35f4efb61cd9953c0
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-26 17:25:51 +0000

    Updated

--- src/me/mayhem/game/entity/player/listeners/input/PlayerMousePressListener.java
+++ src/me/mayhem/game/entity/player/listeners/input/PlayerMousePressListener.java
@@ -1,5 +1,6 @@
 package me.mayhem.game.entity.player.listeners.input;
 
+import me.mayhem.game.entity.player.Player;
 import me.mayhem.input.impl.mouse.MouseButtonPressListener;
 import org.jsfml.window.event.MouseButtonEvent;
 
@@ -7,6 +8,12 @@ import org.jsfml.window.event.MouseButtonEvent;
  * Mouse Listener for the Player Entity
  */
 public class PlayerMousePressListener extends MouseButtonPressListener {
+
+    Player player;
+
+    public PlayerMousePressListener(Player player) {
+        this.player = player;
+    }
 
     @Override
     protected void takeInput(MouseButtonEvent event) {
