2b26699aa5dd8bbc617bac6c762a7de9a1d7855d
j-sammon <j.sammon@lancaster.ac.uk>
2021-01-26 13:37:53 +0000

    adding getstring method

--- src/me/mayhem/screens/nameselectscreen/items/InputBox.java
+++ src/me/mayhem/screens/nameselectscreen/items/InputBox.java
@@ -1,12 +1,13 @@
 package me.mayhem.screens.nameselectscreen.items;
 
+import me.mayhem.util.Vector;
 import me.mayhem.util.ui.impl.TextAreaInteractable;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.system.Vector2f;
 
 public class InputBox extends TextAreaInteractable {
 
-    public InputBox() {
-        super(new RectangleShape(new Vector2f(100, 100)), "fonts/FreeSans.ttf");
+    public InputBox(Vector2f position) {
+        super(new RectangleShape(position), "fonts/FreeSans.ttf");
     }
 }
