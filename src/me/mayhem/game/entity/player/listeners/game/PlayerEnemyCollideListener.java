8f51d645cd1ca62e90bfe2f5f9e31e6b06717504
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-01-26 13:57:46 +0000

    Added ability to stop movement

--- src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
+++ src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
@@ -3,11 +3,15 @@ package me.mayhem.game.entity.player.listeners.game;
 import me.mayhem.game.entity.event.PlayerCollideWithEntityEvent;
 import me.mayhem.game.event.struct.EventListener;
 
+import java.util.concurrent.TimeUnit;
+
 public class PlayerEnemyCollideListener {
 
     @EventListener
     public void onPlayerCollideWithEnemy(PlayerCollideWithEntityEvent event) {
-        event.getPlayer().getMotion().add(0, -10);
+        event.getPlayer().getMotion().add(event.getEntity().getFacing().clone().add(0, -1).multiply(4));
+        event.getEntity().getMotion().add(event.getPlayer().getFacing().clone().multiply(5));
+        event.getEntity().setAttribute("stopMotion", System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(10));
     }
 
 }
