c61b0be97cabd53c3bb67ca9db42067f716f8a6d
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-27 17:50:35 +0000

    Updated MouseReleaseListener

--- src/me/mayhem/game/entity/player/listeners/input/PlayerMouseReleaseListener.java
+++ src/me/mayhem/game/entity/player/listeners/input/PlayerMouseReleaseListener.java
@@ -1,4 +1,24 @@
 package me.mayhem.game.entity.player.listeners.input;
 
-public class PlayerMouseReleaseListener {
+import me.mayhem.game.entity.player.Player;
+import me.mayhem.game.entity.state.EntityState;
+import me.mayhem.input.impl.mouse.MouseButtonReleaseListener;
+import org.jsfml.window.Mouse;
+import org.jsfml.window.event.MouseButtonEvent;
+
+public class PlayerMouseReleaseListener extends MouseButtonReleaseListener {
+
+    private Player player;
+
+    public PlayerMouseReleaseListener(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    protected void takeInput(MouseButtonEvent event) {
+        if (event.asMouseButtonEvent().button == Mouse.Button.LEFT) {
+            this.player.setState(EntityState.NO_MOTION);
+            System.out.println("test 3");
+        }
+    }
 }
