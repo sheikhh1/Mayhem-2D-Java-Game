46cda54ac959fcd8454915bf7d522a84cadc7220
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-27 18:30:14 +0000

    Refactored

--- src/me/mayhem/game/entity/player/listeners/input/PlayerMousePressListener.java
+++ src/me/mayhem/game/entity/player/listeners/input/PlayerMousePressListener.java
@@ -5,21 +5,27 @@ import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.input.impl.mouse.MouseButtonPressListener;
 import org.jsfml.window.Mouse;
 import org.jsfml.window.event.MouseButtonEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Mouse Listener for the Player Entity
  */
 public class PlayerMousePressListener extends MouseButtonPressListener {
 
     private final Player player;
+    private final Map<Mouse.Button, EntityState> mousePressHandler = new HashMap<>();
+
 
     public PlayerMousePressListener(Player player) {
         this.player = player;
+
+        this.mousePressHandler.put(Mouse.Button.LEFT, EntityState.MELEE);
     }
 
     @Override
     protected void takeInput(MouseButtonEvent event) {
-        if (event.asMouseButtonEvent().button == Mouse.Button.LEFT) {
-            this.player.setState(EntityState.MELEE);
-        }
+            this.player.setState(this.mousePressHandler.get(event.asMouseButtonEvent().button));
     }
 }
