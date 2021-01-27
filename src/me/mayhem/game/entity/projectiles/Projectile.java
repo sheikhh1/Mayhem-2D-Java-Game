b85ab17a52b4c09488db6cce15f9515b74f300ef
sheikhh1 <haarissheikh2000@gmail.com>
2021-01-27 14:41:37 +0000

    Added Projectile class

---
+++ src/me/mayhem/game/entity/projectiles/Projectile.java
@@ -0,0 +1,15 @@
+package me.mayhem.game.entity.projectiles;
+
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.collision.impl.ShapeHitbox;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.EntityType;
+import me.mayhem.util.Vector;
+import org.jsfml.graphics.Shape;
+
+public class Projectile extends Entity {
+
+    public Projectile(Shape circle, Vector position) {
+        super(EntityType.PROJECTILE, position, Vector.getZero(), new ShapeHitbox(circle, position, 20, 20), Pathing.NO_PATHING);
+    }
+}
