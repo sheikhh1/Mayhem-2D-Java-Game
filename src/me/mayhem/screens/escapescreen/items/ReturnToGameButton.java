86174a1c566b1845a91569ceef5d6a0523760194
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-01 08:59:42 +0000

    Re-loaded screen & refactored

--- src/me/mayhem/screens/escapescreen/items/ReturnToGameButton.java
+++ src/me/mayhem/screens/escapescreen/items/ReturnToGameButton.java
@@ -2,24 +2,24 @@ package me.mayhem.screens.escapescreen.items;
 
 import me.mayhem.Mayhem;
 import me.mayhem.screens.escapescreen.EscapeScreenManager;
-import me.mayhem.screens.gamescreen.GameScreenManager;
-import me.mayhem.screens.loadpage.LoadPageManager;
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
-import java.awt.*;
-
 public class ReturnToGameButton extends ButtonInteractable {
+
     public ReturnToGameButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "Return to game ");
     }
+
     @Override
     protected void call(RenderWindow window, Event event) {
         EscapeScreenManager screen = (EscapeScreenManager) Mayhem.getCurrentScreen();
+
         if (event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
             screen.unloadScreen(window);
+            screen.loadScreen(Mayhem.getMainWindow());
             Mayhem.setCurrentScreen(screen.getPrevScreen());
         }
     }
