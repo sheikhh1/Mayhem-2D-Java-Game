b13c2129c8da2ba1141d42158d64238be3ade288
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-02 19:33:39 +0000

    Added HoverPathing

---
+++ src/me/mayhem/game/ai/path/impl/HoverPathing.java
@@ -0,0 +1,11 @@
+package me.mayhem.game.ai.path.impl;
+
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.entity.Entity;
+
+public class HoverPathing implements Pathing {
+    @Override
+    public void updatePosition(Entity entity) {
+        
+    }
+}
