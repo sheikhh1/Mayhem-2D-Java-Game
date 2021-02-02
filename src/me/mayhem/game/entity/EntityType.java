a35f1bf4285992356dba7d5165d42ae7fc6d9eb1
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-02 18:33:57 +0000

    Added file image paths to Entity Type

--- src/me/mayhem/game/entity/EntityType.java
+++ src/me/mayhem/game/entity/EntityType.java
@@ -1,22 +1,29 @@
 package me.mayhem.game.entity;
 
+import me.mayhem.util.file.UtilImageLoader;
+import org.jsfml.graphics.Texture;
+
 public enum EntityType {
 
-    PLAYER(200, 1.05f),
-    INFECTED(50,0.8f),
-    CORROSIVE(50,0.8f),
-    FEROCIOUS(100, 0.9f),
-    PROJECTILE(0, 1f),
-    KEYCARD(0, 1f)
+    PLAYER(200, 1.05f, "players/PlayerSheet.png"),
+    INFECTED(50,0.8f,  "enemies/Infected.png"),
+    CORROSIVE(50,0.8f, "enemies/CorrosiveEnemy.png"),
+    FEROCIOUS(100, 0.9f, "enemies/Ferocious.png"),
+    PROJECTILE(0, 1f, "players/PlayerSheet.png"), //TODO: NEED TO ADD CORRECT IMAGE PATH FOR PROJECTILES
+    KEYCARD(0, 1f, "interactables/keycard.png")
 
     ;
 
     private final int maxHealth;
     private final float movementSpeed;
+    private final String imagePath;
+    private final Texture entityTexture;
 
-    EntityType(int maxHealth, float movementSpeed) {
+    EntityType(int maxHealth, float movementSpeed, String imagePath) {
         this.maxHealth = maxHealth;
         this.movementSpeed = movementSpeed;
+        this.imagePath = imagePath;
+        this.entityTexture = UtilImageLoader.loadTextureFromStream(getClass().getClassLoader().getResourceAsStream(this.imagePath));
     }
 
     public int getMaxHealth() {
@@ -26,4 +33,6 @@ public enum EntityType {
     public float getMovementSpeed() {
         return this.movementSpeed;
     }
+
+    
 }
