f72f83fd0ba04318cc19002c4e813af97ec1175b
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-02 20:52:12 +0000

    Added basic movement - Need to clean up

--- src/me/mayhem/game/entity/physics/HoverPhysics.java
+++ src/me/mayhem/game/entity/physics/HoverPhysics.java
@@ -1,9 +1,38 @@
 package me.mayhem.game.entity.physics;
 
-public class HoverPhysics extends EntityPhysics{
+import me.mayhem.game.entity.state.EntityState;
+import me.mayhem.util.Vector;
 
+public class HoverPhysics extends EntityPhysics {
+
+    private static final float DEFAULT_JUMP_STRENGTH = 4f;
+    private Vector motion;
+
+    public void setEntityMotion(Vector motion) {
+        this.motion = motion;
+    }
+
+    /**
+     * Jump method for the Player
+     */
+    @Override
+    public void jump() {
+        this.motion.add(0, -1);
+        System.out.println("jump called");
+    }
+
+    /**
+     * Basic Fall Method Added
+     */
     @Override
     public void fall() {
+        this.motion.add(0, 1);
+        System.out.println("fall called");
+    }
+
+    @Override
+    public void reset(EntityState state) {
 
     }
+
 }
