314d6bad346658cdeaf886dc391091b4fa9be7e2
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-02 19:31:37 +0000

    KeyCard spawned into level

--- src/me/mayhem/game/level/spawning/impl/EasyEntitySpawner.java
+++ src/me/mayhem/game/level/spawning/impl/EasyEntitySpawner.java
@@ -1,9 +1,14 @@
 package me.mayhem.game.level.spawning.impl;
 
 import me.mayhem.game.entity.enemies.infected.InfectedEnemy;
+import me.mayhem.game.entity.keycard.KeyCard;
 import me.mayhem.game.level.Level;
 import me.mayhem.game.level.spawning.EntitySpawner;
 import me.mayhem.util.Vector;
+import org.jsfml.graphics.RectangleShape;
+import org.jsfml.system.Vector2f;
+
+import java.security.Key;
 
 public class EasyEntitySpawner implements EntitySpawner {
 
@@ -15,5 +20,7 @@ public class EasyEntitySpawner implements EntitySpawner {
         for (Vector vector : level.getDifficulty().getGenerator().getEnemySpawnPositions()) {
             level.spawnEntity(new InfectedEnemy(vector.clone(), level));
         }
+
+        level.spawnEntity(new KeyCard(level.getDifficulty().getGenerator().getKeyCardSpawnPosition()));
     }
 }
