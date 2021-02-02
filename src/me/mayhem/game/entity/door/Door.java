4555b55140a09a3aab5e8c3c5372dd62ef289e79
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-02 13:35:56 +0000

    Cleaned up Door

--- src/me/mayhem/game/entity/door/Door.java
+++ src/me/mayhem/game/entity/door/Door.java
@@ -1,43 +1,27 @@
 package me.mayhem.game.entity.door;
 
-import me.mayhem.Mayhem;
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.attribute.Attribute;
-import me.mayhem.game.collision.Hitbox;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
 import me.mayhem.util.Vector;
+import me.mayhem.util.file.UtilSprite;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Sprite;
-import org.jsfml.graphics.Texture;
-
-import java.io.IOException;
 
 public class Door extends Entity {
-    private Sprite sprite;
 
-    public Door(EntityType type, Vector position, Vector motion, String image, Hitbox hitbox, Pathing pathing, Attribute<?>... attributes) {
+    private final Sprite sprite;
+
+    public Door(Vector position, String image, Attribute<?>... attributes) {
         super(EntityType.DOOR, position, Vector.ZERO, new SpriteHitbox(position, 0, 0), Pathing.NO_PATHING, attributes);
 
-        this.sprite = this.loadFromPath(image);
+        this.sprite = UtilSprite.loadFromPath(image);
     }
-        @Override
-        public void update(RenderWindow window) {
-            window.draw(this.sprite);
-        }
-    public Sprite loadFromPath(String path) {
-        Texture newTexture = new Texture();
-        Sprite sprite = new Sprite();
 
-        try {
-            newTexture.loadFromStream(Mayhem.class.getClassLoader().getResourceAsStream(path));
-            sprite.setTexture(newTexture);
-            return sprite;
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        return null;
+    @Override
+    public void update(RenderWindow window) {
+        window.draw(this.sprite);
     }
 }
