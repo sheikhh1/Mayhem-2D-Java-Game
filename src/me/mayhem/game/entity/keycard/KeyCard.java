20941c4cf5256dd76f85364618f17ba0d8d1a5d3
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-02 19:31:09 +0000

    Added basic keyCard class

--- src/me/mayhem/game/entity/keycard/KeyCard.java
+++ src/me/mayhem/game/entity/keycard/KeyCard.java
@@ -1,4 +1,31 @@
 package me.mayhem.game.entity.keycard;
 
-public class KeyCard {
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.collision.impl.SpriteHitbox;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.EntityType;
+import me.mayhem.game.entity.state.EntityState;
+import me.mayhem.util.Vector;
+import org.jsfml.graphics.*;
+import org.jsfml.system.Vector2f;
+
+public class KeyCard extends Entity {
+
+    private RectangleShape keyCard;
+
+    public KeyCard(Vector position) {
+        super(EntityType.KEYCARD, position, Vector.getZero(), new SpriteHitbox(position, 31, 31), Pathing.NO_PATHING);
+        this.getEntityPhysics().setEntityMotion(this.getMotion());
+        this.setState(EntityState.NO_MOTION);
+        this.keyCard = new RectangleShape(new Vector2f(31, 31));
+        this.keyCard.setPosition(position.toVector());
+        this.keyCard.setTexture(EntityType.KEYCARD.getEntityTexture());
+    }
+
+    @Override
+    public void update(RenderWindow renderWindow) {
+       renderWindow.draw(this.keyCard);
+    }
+
+
 }
