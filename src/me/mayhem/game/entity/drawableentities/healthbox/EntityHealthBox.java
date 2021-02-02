9cb0873029531eb64af7cb51b53bbeb3093c94e6
j-sammon <j.sammon@lancaster.ac.uk>
2021-02-02 15:13:54 +0000

    work on drawing methods

--- src/me/mayhem/game/entity/drawableentities/healthbox/EntityHealthBox.java
+++ src/me/mayhem/game/entity/drawableentities/healthbox/EntityHealthBox.java
@@ -1,6 +1,9 @@
 package me.mayhem.game.entity.drawableentities.healthbox;
 
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.EntityType;
 import me.mayhem.game.entity.drawable.drawablethings.DrawableSquare;
 import me.mayhem.util.Vector;
 import org.jsfml.graphics.Color;
@@ -18,23 +21,28 @@ public class EntityHealthBox {
 
 
     public EntityHealthBox(Vector entityPosition){
-        rePosition(entityPosition);
+
+        totalHealth = createHealthBox(new Vector(entityPosition.getX() - 25, entityPosition.getY() - 30));
+        currentHealth = createCurretHealth(new Vector(entityPosition.getX()- 25, entityPosition.getY() - 30));
 
     }
 
     private RectangleShape createHealthBox(Vector position){
         RectangleShape rect = new RectangleShape(position.toVector());
+
+        rect.setPosition(position.toVector());
         rect.setSize(new Vector(HEALTHBOXWIDTH,HEALTHBOXHEIGHT).toVector());
         rect.setFillColor(Color.RED);
+
         return rect;
-
-
-
     }
     private RectangleShape createCurretHealth(Vector position){
-        RectangleShape rect = new RectangleShape(position.toVector());
+        RectangleShape rect = new RectangleShape();
+
+        rect.setPosition(position.toVector());
         rect.setSize(new Vector(HEALTHBOXWIDTH,HEALTHBOXHEIGHT).toVector());
         rect.setFillColor(Color.GREEN);
+
         return rect;
     }
 
@@ -43,20 +51,24 @@ public class EntityHealthBox {
         float newsize = calculateSize( (entity.getHealth()), entity.getType().getMaxHealth());
 
         currentHealth.setSize( new Vector(newsize, HEALTHBOXHEIGHT).toVector());
-
-
     }
     public void rePosition(Vector entityPosition){
-        totalHealth = createHealthBox(new Vector(entityPosition.getX(), entityPosition.getY() - 30));
-        currentHealth = createCurretHealth(new Vector(entityPosition.getX(), entityPosition.getY() - 30));
+
+
+        totalHealth.setPosition(new Vector(entityPosition.getX()- 25, entityPosition.getY() - 30).toVector());
+        currentHealth.setPosition(new Vector(entityPosition.getX()- 25, entityPosition.getY() - 30).toVector());
+
     }
 
-    public void draw(RenderWindow window){
+    public void draw(RenderWindow window, Entity entity){
 
+        rePosition(entity.getPosition());
         window.draw(totalHealth);
         window.draw(currentHealth);
     }
+
     public int calculateSize(double currentHealth, double maxHealth){
+
         return (int) ((currentHealth/maxHealth) * 100);// returns the health as a percentage to be drawn
     }
 
