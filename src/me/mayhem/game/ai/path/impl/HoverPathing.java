be72614562f0f1b1979aab88b9ca9b1c3b02374e
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-02 20:51:17 +0000

    Added function call counters

--- src/me/mayhem/game/ai/path/impl/HoverPathing.java
+++ src/me/mayhem/game/ai/path/impl/HoverPathing.java
@@ -2,10 +2,27 @@ package me.mayhem.game.ai.path.impl;
 
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.state.EntityState;
 
 public class HoverPathing implements Pathing {
+
+    private int updateFunctionCounter = 0;
+
     @Override
     public void updatePosition(Entity entity) {
+        updateFunctionCounter++;
+
+        if (updateFunctionCounter <= 50) {
+            entity.setJumping(true);
+            entity.setFalling(false);
+            System.out.println("called 1");
+        } else {
+            entity.setJumping(false);
+            entity.setFalling(true);
+            System.out.println("called 2");
+        }
+
+        if (updateFunctionCounter == 100) updateFunctionCounter = 0;
 
     }
 }
