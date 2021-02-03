df78e6cdd6957d9b96b433e080370f3ec569a146
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-03 15:55:56 +0000

    Refactored & cleaned up health box

--- src/me/mayhem/game/entity/drawableentities/healthbox/EntityHealthBox.java
+++ src/me/mayhem/game/entity/drawableentities/healthbox/EntityHealthBox.java
@@ -7,58 +7,55 @@ import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.RenderWindow;
 
 public class EntityHealthBox {
-    public static final int HEALTHBOXWIDTH = 50;
-    public static final int HEALTHBOXHEIGHT = 10;
 
-    private RectangleShape totalHealth;
-    private RectangleShape currentHealth;
+    private static final int HEALTH_BOX_WIDTH = 50;
+    private static final int HEALTH_BOX_HEIGHT = 10;
 
+    private final RectangleShape totalHealth;
+    private final RectangleShape currentHealth;
 
-    public EntityHealthBox(Vector entityPosition){
-
-        totalHealth = createHealthBox(new Vector(entityPosition.getX() - 25, entityPosition.getY() - 30));
-        currentHealth = createCurretHealth(new Vector(entityPosition.getX()- 25, entityPosition.getY() - 30));
-
+    public EntityHealthBox(Vector entityPosition) {
+        this.totalHealth = this.createHealthBox(new Vector(entityPosition.getX() - 25, entityPosition.getY() - 30), Color.RED);
+        this.currentHealth = this.createHealthBox(new Vector(entityPosition.getX() - 25, entityPosition.getY() - 30), Color.GREEN);
     }
 
-    private RectangleShape createHealthBox(Vector position){
-        RectangleShape rect = new RectangleShape(position.toVector());
+    private RectangleShape createHealthBox(Vector position, Color fillColour) {
+        RectangleShape rectangleShape = new RectangleShape(position.toVector());
 
-        rect.setPosition(position.toVector());
-        rect.setSize(new Vector(HEALTHBOXWIDTH,HEALTHBOXHEIGHT).toVector());
-        rect.setFillColor(Color.RED);
+        rectangleShape.setPosition(position.toVector());
+        rectangleShape.setSize(new Vector(HEALTH_BOX_WIDTH, HEALTH_BOX_HEIGHT).toVector());
+        rectangleShape.setFillColor(fillColour);
 
-        return rect;
-    }
-    private RectangleShape createCurretHealth(Vector position){
-        RectangleShape rect = new RectangleShape();
-
-        rect.setPosition(position.toVector());
-        rect.setSize(new Vector(HEALTHBOXWIDTH,HEALTHBOXHEIGHT).toVector());
-        rect.setFillColor(Color.GREEN);
-
-        return rect;
+        return rectangleShape;
     }
 
-    public void Redraw(Entity entity){
+    public void redrawHealthBars(Entity entity) {
+        float newSize = this.calculateSize((entity.getHealth()), entity.getType().getMaxHealth());
 
-        float newsize = calculateSize( (entity.getHealth()), entity.getType().getMaxHealth());
-
-        currentHealth.setSize( new Vector(newsize, HEALTHBOXHEIGHT).toVector());
-    }
-    public void rePosition(Entity entity){
-        totalHealth.setPosition(entity.getCenter().add(-totalHealth.getGlobalBounds().width / 2.3f, -40).toVector());
-        currentHealth.setPosition(entity.getCenter().add(-totalHealth.getGlobalBounds().width / 2.3f, -40).toVector());
+        this.currentHealth.setSize(new Vector(newSize, HEALTH_BOX_HEIGHT).toVector());
     }
 
-    public void draw(RenderWindow window, Entity entity){
-        rePosition(entity);
-        window.draw(totalHealth);
-        window.draw(currentHealth);
+    public void updatePosition(Entity entity) {
+        this.totalHealth.setPosition(entity.getCenter().add(-this.totalHealth.getGlobalBounds().width / 2.3f, -40).toVector());
+        this.currentHealth.setPosition(entity.getCenter().add(-this.totalHealth.getGlobalBounds().width / 2.3f, -40).toVector());
     }
 
-    public int calculateSize(double currentHealth, double maxHealth){
-        return (int) ((currentHealth/maxHealth) * 100);// returns the health as a percentage to be drawn
+    public void draw(RenderWindow window, Entity entity) {
+        this.updatePosition(entity);
+
+        window.draw(this.totalHealth);
+        window.draw(this.currentHealth);
     }
 
+    /**
+     *
+     * Calculates the percentage health of the max health
+     *
+     * @param currentHealth the current health of the {@link Entity}
+     * @param maxHealth the maximum health of the {@link Entity}
+     * @return the percentage health
+     */
+    public int calculateSize(double currentHealth, double maxHealth) {
+        return (int) ((currentHealth / maxHealth) * 100);
+    }
 }
