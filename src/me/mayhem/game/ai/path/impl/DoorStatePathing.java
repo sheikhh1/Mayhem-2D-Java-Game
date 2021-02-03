1540b057449c42768ac2ff77c06c7b4651d09a71
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-03 17:03:14 +0000

    Basic Door pathing working

--- src/me/mayhem/game/ai/path/impl/DoorStatePathing.java
+++ src/me/mayhem/game/ai/path/impl/DoorStatePathing.java
@@ -2,7 +2,9 @@ package me.mayhem.game.ai.path.impl;
 
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.door.DoorEnum;
 import me.mayhem.game.level.Level;
+import me.mayhem.util.Vector;
 
 public class DoorStatePathing implements Pathing {
 
@@ -14,6 +16,15 @@ public class DoorStatePathing implements Pathing {
 
     @Override
     public void updatePosition(Entity entity) {
+        Vector toPlayer = this.currentLevel.getPlayer().getPosition().clone().subtract(entity.getPosition());
+        System.out.println(Math.abs(toPlayer.getX()));
+        if (Math.abs(toPlayer.getX()) < 200) {
+            entity.setTexture(DoorEnum.OPEN.getDoorTexture());
+        } else if (Math.abs(toPlayer.getX()) < 400) {
+            entity.setTexture(DoorEnum.OPENING.getDoorTexture());
+        } else {
+            entity.setTexture(DoorEnum.CLOSED.getDoorTexture());
 
+        }
     }
 }
