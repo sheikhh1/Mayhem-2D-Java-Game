3b5ae9374691d18e677f3ed5273bf72f63b0a4d0
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-03 14:46:58 +0000

    Updated Door Constructor

--- src/me/mayhem/game/entity/Door.java
+++ src/me/mayhem/game/entity/Door.java
@@ -1,13 +1,29 @@
 package me.mayhem.game.entity;
 
+
 import me.mayhem.game.ai.path.Pathing;
-import me.mayhem.game.attribute.Attribute;
-import me.mayhem.game.collision.Hitbox;
+import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.util.Vector;
+import org.jsfml.graphics.RectangleShape;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.system.Vector2f;
 
 public class Door extends Entity {
-    
-    public Door(EntityType type, Vector position, Vector motion, Hitbox hitbox, Pathing pathing, Attribute<?>... attributes) {
-        super(type, position, motion, hitbox, pathing, attributes);
+
+    private RectangleShape mainDoor;
+    private Vector doorPosition;
+
+    public Door(Vector position) {
+        super(EntityType.DOOR, position, Vector.getZero(), new SpriteHitbox(position,50,50), Pathing.NO_PATHING);
+        this.doorPosition = position;
+        this.mainDoor = new RectangleShape(new Vector2f(50,50));
+        this.mainDoor.setPosition(position.toVector());
+        this.mainDoor.setTexture(EntityType.DOOR.getEntityTexture());
+    }
+
+    @Override
+    public void update(RenderWindow renderWindow) {
+        this.mainDoor.setPosition(this.doorPosition.toVector());
+        renderWindow.draw(this.mainDoor);
     }
 }
