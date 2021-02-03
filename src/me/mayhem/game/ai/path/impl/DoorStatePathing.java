e70270f27fc4886fe20b98baf97e2d1fb2164646
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-03 15:17:50 +0000

    Created DoorStatePathing

---
+++ src/me/mayhem/game/ai/path/impl/DoorStatePathing.java
@@ -0,0 +1,17 @@
+package me.mayhem.game.ai.path.impl;
+
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.level.Level;
+
+public class DoorStatePathing implements Pathing {
+
+    public DoorStatePathing(Level currentLevel) {
+        
+    }
+
+    @Override
+    public void updatePosition(Entity entity) {
+
+    }
+}
