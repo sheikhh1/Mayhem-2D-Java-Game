93c6de071a4d1468100abeb455bc1b124bf37ad5
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-03 22:16:44 +0000

    Created PlayerInteract Interface

---
+++ src/me/mayhem/game/entity/player/PlayerInteract.java
@@ -0,0 +1,15 @@
+package me.mayhem.game.entity.player;
+
+import me.mayhem.game.entity.Entity;
+
+/**
+ * Interface created for player to attack enemies and interact with doors / keycards
+ */
+public interface PlayerInteract {
+
+    /**
+     * Attack an enemy
+     * @param enemy
+     */
+    void attack(Entity enemy);
+}
