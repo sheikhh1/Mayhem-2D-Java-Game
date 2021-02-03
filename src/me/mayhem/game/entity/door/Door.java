816fe962bc61f19e366d6f5b9620d4526788c6df
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-03 17:04:23 +0000

    Added setTexture method

--- src/me/mayhem/game/entity/door/Door.java
+++ src/me/mayhem/game/entity/door/Door.java
@@ -5,10 +5,12 @@ import me.mayhem.game.ai.path.impl.DoorStatePathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
+import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
 import org.jsfml.system.Vector2f;
 
 public class Door extends Entity {
@@ -19,10 +21,16 @@ public class Door extends Entity {
     public Door(Vector position, Level level) {
         super(EntityType.DOOR, position, Vector.getZero(), new SpriteHitbox(position,130,130), new DoorStatePathing(level));
         this.doorPosition = position;
+        this.setState(EntityState.NO_MOTION);
         this.getEntityPhysics().setEntityMotion(this.getMotion());
         this.mainDoor = new RectangleShape(new Vector2f(130,130));
         this.mainDoor.setPosition(position.toVector());
-        this.mainDoor.setTexture(EntityType.DOOR.getEntityTexture());
+        this.mainDoor.setTexture(DoorEnum.CLOSED.getDoorTexture());
+    }
+
+    @Override
+    public void setTexture(Texture doorTexture) {
+        this.mainDoor.setTexture(doorTexture);
     }
 
     @Override
