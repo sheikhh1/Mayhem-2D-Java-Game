5ea921b533c3a7c3b570fcb68053286afd9177af
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-03 18:21:54 +0000

    Refactored

--- src/me/mayhem/game/entity/obstacles/Obstacle.java
+++ src/me/mayhem/game/entity/obstacles/Obstacle.java
@@ -1,6 +1,5 @@
 package me.mayhem.game.entity.obstacles;
 
-import me.mayhem.Mayhem;
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
@@ -8,11 +7,9 @@ import me.mayhem.game.entity.EntityType;
 import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
+import me.mayhem.util.file.UtilSprite;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Sprite;
-import org.jsfml.graphics.Texture;
-
-import java.io.IOException;
 
 public class Obstacle extends Entity {
 
@@ -21,7 +18,7 @@ public class Obstacle extends Entity {
     public Obstacle(Vector position, Level level) {
         super(EntityType.SPIKES, position, Vector.getZero(), new SpriteHitbox(position, 48,48 ), Pathing.NO_PATHING);
 
-        sprite = this.loadFromPath("obstacles/Spike.png");
+        sprite = UtilSprite.loadFromPath("obstacles/Spike.png");
         sprite.setPosition(position.toVector());
 
         //this.animate.setSpritePosition(position.toVector());
@@ -41,20 +38,5 @@ public class Obstacle extends Entity {
     @Override
     public void update(RenderWindow r) {
         r.draw(this.sprite);
-    }
-
-    public Sprite loadFromPath(String path) { // from KeyCard
-        Texture newTexture = new Texture();
-        Sprite sprite = new Sprite();
-
-        try {
-            newTexture.loadFromStream(Mayhem.class.getClassLoader().getResourceAsStream(path));
-            sprite.setTexture(newTexture);
-            return sprite;
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        return null;
     }
 }
