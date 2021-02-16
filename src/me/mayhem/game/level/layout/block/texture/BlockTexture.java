f5b02e86421105bb2a07f021fa787e32a940aef7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 18:08:57 +0000

    Added wall damage texture

--- src/me/mayhem/game/level/layout/block/texture/BlockTexture.java
+++ src/me/mayhem/game/level/layout/block/texture/BlockTexture.java
@@ -11,6 +11,7 @@ public enum BlockTexture {
     LAVA(RGB.of(255, 63, 0), UtilSprite.loadFromPath("blocks/lava.png", 1.6f, 1.6f)),
     SPEED_UP_RIGHT(RGB.of(138, 138, 138), UtilSprite.loadFromPath("blocks/speedup_right.png", 1.6f, 1.6f)),
     SPEED_UP_LEFT(RGB.of(138, 138, 0), UtilSprite.loadFromPath("blocks/speedup_left.png", 1.6f, 1.6f)),
+    WALL_DAMAGE(RGB.of(255, 125, 125), UtilSprite.loadFromPath("blocks/walldamage.png", 1.6f, 1.6f)),
 
     ;
 
