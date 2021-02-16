4085e03e6e5c84a797d50c37f046f9459c61d0e5
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 16:33:00 +0000

    Changed BouncyBlock to use sprites

--- src/me/mayhem/game/level/layout/block/types/BouncyBlock.java
+++ src/me/mayhem/game/level/layout/block/types/BouncyBlock.java
@@ -1,18 +1,18 @@
 package me.mayhem.game.level.layout.block.types;
 
 import me.mayhem.game.collision.Hitbox;
-import me.mayhem.game.collision.impl.ShapeHitbox;
+import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.physics.EntityPhysics;
 import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.layout.block.Block;
 import me.mayhem.util.Vector;
-import org.jsfml.graphics.Shape;
+import org.jsfml.graphics.Sprite;
 
 public class BouncyBlock extends Block {
 
-    public BouncyBlock(Vector position, Shape drawable, Hitbox hitbox) {
-        super(position, drawable, hitbox);
+    public BouncyBlock(Vector position, Sprite sprite, Hitbox hitbox) {
+        super(position, sprite, hitbox);
     }
 
     @Override
@@ -28,15 +28,11 @@ public class BouncyBlock extends Block {
     public static final class Builder extends Block.Builder {
         @Override
         public Block build() {
-            this.drawable.setFillColor(this.fillColor);
-            this.drawable.setOutlineColor(this.outlineColor);
-            this.drawable.setPosition(this.position.toVector());
-
             if (this.hitbox == null) {
-                this.hitbox = new ShapeHitbox(this.drawable, this.position, this.height, this.width);
+                this.hitbox = new SpriteHitbox(this.position, this.height, this.width);
             }
 
-            return new BouncyBlock(this.position, this.drawable, this.hitbox);
+            return new BouncyBlock(this.position, this.sprite, this.hitbox);
         }
     }
 }
