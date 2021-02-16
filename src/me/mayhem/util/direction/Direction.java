ad609eb3a92b1701479a4196fab61b239bbf529e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 17:25:11 +0000

    Added Direction enum for checking above and below

---
+++ src/me/mayhem/util/direction/Direction.java
@@ -0,0 +1,31 @@
+package me.mayhem.util.direction;
+
+import me.mayhem.util.Vector;
+
+import java.util.function.BiFunction;
+
+public enum Direction {
+
+    ABOVE((a, b) -> {
+        Vector between = a.subtract(b).normalize();
+
+        return between.getY() > 0;
+    }),
+    BELOW((a, b) -> {
+        Vector between = a.subtract(b).normalize();
+
+        return between.getY() < 0;
+    }),
+
+    ;
+
+    private final BiFunction<Vector, Vector, Boolean> checker;
+
+    Direction(BiFunction<Vector, Vector, Boolean> checker) {
+        this.checker = checker;
+    }
+
+    public boolean is(Vector a, Vector b) {
+        return this.checker.apply(a.clone(), b);
+    }
+}
