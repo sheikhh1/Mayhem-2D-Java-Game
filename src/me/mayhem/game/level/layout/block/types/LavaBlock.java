56a514c207fe18793862197056c47d586029ac40
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 16:58:04 +0000

    Added LavaBlock

---
+++ src/me/mayhem/game/level/layout/block/types/LavaBlock.java
@@ -0,0 +1,36 @@
+package me.mayhem.game.level.layout.block.types;
+
+import me.mayhem.game.collision.Hitbox;
+import me.mayhem.game.collision.impl.SpriteHitbox;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.game.level.layout.block.texture.BlockTexture;
+import me.mayhem.util.Vector;
+import org.jsfml.graphics.Sprite;
+
+public class LavaBlock extends Block {
+
+    public LavaBlock(Vector position, Sprite sprite, Hitbox hitbox, int width, int height) {
+        super(position, sprite, hitbox, width, height);
+    }
+
+    @Override
+    public void onCollide(Entity entity) {
+        entity.damage(entity, 20f);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder extends Block.Builder {
+        @Override
+        public Block build() {
+            if (this.hitbox == null) {
+                this.hitbox = new SpriteHitbox(this.position, this.height, this.width);
+            }
+
+            return new LavaBlock(this.position, BlockTexture.LAVA.getSprite(), this.hitbox, this.width, this.height);
+        }
+    }
+}
