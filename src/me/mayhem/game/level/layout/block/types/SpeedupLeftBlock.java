ff4c91ed4b841fffe38f65007596fcb3a28ad348
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 17:34:49 +0000

    Added speedup left block

---
+++ src/me/mayhem/game/level/layout/block/types/SpeedupLeftBlock.java
@@ -0,0 +1,41 @@
+package me.mayhem.game.level.layout.block.types;
+
+import me.mayhem.game.collision.Hitbox;
+import me.mayhem.game.collision.impl.SpriteHitbox;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.game.level.layout.block.texture.BlockTexture;
+import me.mayhem.util.Vector;
+import me.mayhem.util.direction.Direction;
+import org.jsfml.graphics.Sprite;
+
+public class SpeedupLeftBlock extends Block {
+
+    public SpeedupLeftBlock(Vector position, Sprite sprite, Hitbox hitbox, int width, int height) {
+        super(position, sprite, hitbox, width, height);
+    }
+
+    @Override
+    public void onCollide(Entity entity) {
+        if (!Direction.ABOVE.is(entity.getPosition(), this.getPosition())) {
+            return;
+        }
+
+        entity.getMotion().add(entity.getFacing().multiply(2));
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder extends Block.Builder {
+        @Override
+        public Block build() {
+            if (this.hitbox == null) {
+                this.hitbox = new SpriteHitbox(this.position, this.height, this.width);
+            }
+
+            return new SpeedupLeftBlock(this.position, BlockTexture.SPEED_UP_LEFT.getSprite(), this.hitbox, this.width, this.height);
+        }
+    }
+}
