87435afdeee2a716e981751f3b907d1924ee2f54
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 17:11:16 +0000

    Added speedup texture

--- src/me/mayhem/game/level/layout/block/texture/BlockTexture.java
+++ src/me/mayhem/game/level/layout/block/texture/BlockTexture.java
@@ -9,6 +9,7 @@ public enum BlockTexture {
     BASIC(RGB.of(255, 255, 255), UtilSprite.loadFromPath("blocks/default.png", 1.6f, 1.6f)),
     BOUNCY(RGB.of(255, 255, 255), UtilSprite.loadFromPath("blocks/bouncy.png", 1.6f, 1.6f)),
     LAVA(RGB.of(255, 63, 0), UtilSprite.loadFromPath("blocks/lava.png", 1.6f, 1.6f)),
+    SPEED_UP(RGB.of(138, 138, 138), UtilSprite.loadFromPath("blocks/speedup.png", 1.6f, 1.6f)),
 
     ;
 
