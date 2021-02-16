201f89598f2fd9e0cbbaec30b5fe6df19592bc51
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 13:56:43 +0000

    Refactored keycard

--- src/me/mayhem/game/entity/entities/friendly/keycard/KeyCard.java
+++ src/me/mayhem/game/entity/entities/friendly/keycard/KeyCard.java
@@ -1,4 +1,4 @@
-package me.mayhem.game.entity.keycard;
+package me.mayhem.game.entity.entities.friendly.keycard;
 
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
