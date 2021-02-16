8d052b7712ad4436a1d0b6d14a2bb94569d5c34a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 16:34:31 +0000

    Added BlockTexture enum

---
+++ src/me/mayhem/game/level/layout/block/texture/BlockTexture.java
@@ -0,0 +1,28 @@
+package me.mayhem.game.level.layout.block.texture;
+
+import me.mayhem.util.RGB;
+import me.mayhem.util.file.UtilSprite;
+import org.jsfml.graphics.Sprite;
+
+public enum BlockTexture {
+
+    BASIC(RGB.of(255, 255, 255), UtilSprite.loadFromPath("blocks/default.png", 1.6f, 1.6f)),
+
+    ;
+
+    private final RGB rgb;
+    private final Sprite sprite;
+
+    BlockTexture(RGB rgb, Sprite sprite) {
+        this.rgb = rgb;
+        this.sprite = sprite;
+    }
+
+    public RGB getRgb() {
+        return this.rgb;
+    }
+
+    public Sprite getSprite() {
+        return this.sprite;
+    }
+}
