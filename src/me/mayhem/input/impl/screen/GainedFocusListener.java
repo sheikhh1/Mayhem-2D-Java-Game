91332fefaddc1b8e59b9af495a43b8209669482d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-16 14:30:02 +0000

    Fixed typing

--- src/me/mayhem/input/impl/screen/GainedFocusListener.java
+++ src/me/mayhem/input/impl/screen/GainedFocusListener.java
@@ -2,9 +2,8 @@ package me.mayhem.input.impl.screen;
 
 import me.mayhem.input.InputListener;
 import org.jsfml.window.event.Event;
-import org.jsfml.window.event.MouseEvent;
 
-public abstract class GainedFocusListener extends InputListener<MouseEvent> {
+public abstract class GainedFocusListener extends InputListener<Event> {
 
     public GainedFocusListener() {
         super(Event.Type.GAINED_FOCUS);
--- src/me/mayhem/input/impl/screen/LostFocusListener.java
+++ src/me/mayhem/input/impl/screen/LostFocusListener.java
@@ -2,9 +2,8 @@ package me.mayhem.input.impl.screen;
 
 import me.mayhem.input.InputListener;
 import org.jsfml.window.event.Event;
-import org.jsfml.window.event.MouseEvent;
 
-public abstract class LostFocusListener extends InputListener<MouseEvent> {
+public abstract class LostFocusListener extends InputListener<Event> {
 
     public LostFocusListener() {
         super(Event.Type.LOST_FOCUS);
