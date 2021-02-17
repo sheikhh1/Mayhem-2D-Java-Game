52946bdf962a09d2fb3e571808773e2e2286d69e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 13:08:20 +0000

    Started EntityDeathListener

---
+++ src/me/mayhem/game/entity/listener/EntityDeathListener.java
@@ -0,0 +1,12 @@
+package me.mayhem.game.entity.listener;
+
+import me.mayhem.game.GameManager;
+
+public class EntityDeathListener {
+
+    private final GameManager gameManager;
+
+    public EntityDeathListener(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+}
