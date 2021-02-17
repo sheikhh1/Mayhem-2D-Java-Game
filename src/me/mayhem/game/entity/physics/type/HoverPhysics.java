3206727ba33857ba0c05f9acc47b21da1fd23fa8
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 12:52:20 +0000

    Created matching constructor

--- src/me/mayhem/game/entity/physics/type/HoverPhysics.java
+++ src/me/mayhem/game/entity/physics/type/HoverPhysics.java
@@ -1,10 +1,16 @@
 package me.mayhem.game.entity.physics.type;
 
+import me.mayhem.game.entity.EntityType;
 import me.mayhem.game.entity.physics.EntityPhysics;
+import me.mayhem.util.Vector;
 
 public class HoverPhysics extends EntityPhysics {
 
     private static final float HOVER_STRENGTH = 1f;
+
+    public HoverPhysics(EntityType entityType, Vector motion) {
+        super(entityType, motion);
+    }
 
     @Override
     public void jump() {
