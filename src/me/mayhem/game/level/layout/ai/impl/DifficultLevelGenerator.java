f644186e93935197d4f2c97393ce08d96743d497
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 14:41:30 +0000

    Fixed difficult typing

--- src/me/mayhem/game/level/layout/ai/impl/DifficultLevelGenerator.java
+++ src/me/mayhem/game/level/layout/ai/impl/DifficultLevelGenerator.java
@@ -2,6 +2,7 @@ package me.mayhem.game.level.layout.ai.impl;
 
 import me.mayhem.game.level.layout.ai.LevelGenerator;
 import me.mayhem.game.level.layout.block.Block;
+import me.mayhem.game.level.spawning.SpawnPosition;
 import me.mayhem.util.Vector;
 
 import java.util.Collections;
@@ -29,7 +30,7 @@ public class DifficultLevelGenerator implements LevelGenerator {
     }
 
     @Override
-    public List<Vector> getEnemySpawnPositions() {
+    public List<SpawnPosition> getEnemySpawnPositions() {
         return null;
     }
 }
