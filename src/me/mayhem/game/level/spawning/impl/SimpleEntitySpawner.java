2088491abd83aefac3defca5be8a623106274427
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 14:41:01 +0000

    Fixed SimpleEntitySpawner

--- src/me/mayhem/game/level/spawning/impl/SimpleEntitySpawner.java
+++ src/me/mayhem/game/level/spawning/impl/SimpleEntitySpawner.java
@@ -1,22 +1,15 @@
 package me.mayhem.game.level.spawning.impl;
 
-import me.mayhem.game.entity.entities.collect.key.KeyCard;
-import me.mayhem.game.entity.entities.enemies.infected.InfectedEnemy;
-import me.mayhem.game.entity.entities.friendly.door.Door;
 import me.mayhem.game.level.Level;
 import me.mayhem.game.level.spawning.EntitySpawner;
-import me.mayhem.util.Vector;
+import me.mayhem.game.level.spawning.SpawnPosition;
 
 public class SimpleEntitySpawner implements EntitySpawner {
 
     @Override
     public void spawnEntities(Level level) {
-        for (Vector vector : level.getDifficulty().getGenerator().getEnemySpawnPositions()) {
-            level.spawnEntity(new InfectedEnemy(vector.clone(), level));
+        for (SpawnPosition position : level.getDifficulty().getGenerator().getEnemySpawnPositions()) {
+            level.spawnEntity(position.getSpawnType().apply(position.getPosition(), level));
         }
-
-        level.spawnEntity(new KeyCard(level.getDifficulty().getGenerator().getKeyCardSpawnPosition()));
-
-        level.spawnEntity(new Door(level.getDifficulty().getGenerator().getDoorPosition(), level));
     }
 }
