72c915154b51b9c4bfd7223f70fef25e572045c0
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 12:48:29 +0000

    Removed setMotion unnecessary and removed silly variable

--- src/me/mayhem/game/entity/entities/friendly/keycard/KeyCard.java
+++ src/me/mayhem/game/entity/entities/friendly/keycard/KeyCard.java
@@ -13,13 +13,11 @@ import org.jsfml.system.Vector2f;
 public class KeyCard extends Entity {
 
     private RectangleShape keyCard;
-    private Vector keyCardPosition;
 
     public KeyCard(Vector position) {
         super(EntityType.KEY_CARD, position, Vector.getZero(), new SpriteHitbox(position, 31, 31), Pathing.HOVER_PATHING);
         super.entityPhysics = new HoverPhysics();
-        this.keyCardPosition = position;
-        this.getEntityPhysics().setEntityMotion(this.getMotion());
+
         this.keyCard = new RectangleShape(new Vector2f(31, 31));
         this.keyCard.setPosition(position.toVector());
         this.keyCard.setTexture(EntityType.KEY_CARD.getEntityTexture());
@@ -27,9 +25,7 @@ public class KeyCard extends Entity {
 
     @Override
     public void update(RenderWindow renderWindow) {
-        this.keyCard.setPosition(this.keyCardPosition.toVector());
+        this.keyCard.setPosition(this.getPosition().toVector());
         renderWindow.draw(this.keyCard);
     }
-
-
 }
