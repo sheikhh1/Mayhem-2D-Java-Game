adabead91ea43da5919848214330838969527ce9
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 13:25:24 +0000

    Implemented collectability

--- src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
+++ src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
@@ -1,5 +1,6 @@
 package me.mayhem.game.entity.player.listeners.game;
 
+import me.mayhem.game.entity.entities.collectable.Collectable;
 import me.mayhem.game.entity.entities.enemies.Enemy;
 import me.mayhem.game.entity.event.PlayerCollideWithEntityEvent;
 import me.mayhem.game.event.struct.EventListener;
@@ -10,6 +11,8 @@ public class PlayerEnemyCollideListener {
     public void onPlayerCollideWithEnemy(PlayerCollideWithEntityEvent event) {
         if (event.getEntity() instanceof Enemy) {
             ((Enemy) event.getEntity()).attack(event.getPlayer());
+        } else if (event.getEntity() instanceof Collectable) {
+            ((Collectable) event.getEntity()).collected(event.getPlayer());
         }
     }
 }
