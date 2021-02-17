62311e3fb70dfe964f4adedf38b2de0580136417
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 13:22:59 +0000

    Created Collectable interface

---
+++ src/me/mayhem/game/entity/entities/collectable/Collectable.java
@@ -0,0 +1,15 @@
+package me.mayhem.game.entity.entities.collectable;
+
+import me.mayhem.game.entity.player.Player;
+
+public interface Collectable {
+
+    /**
+     *
+     * Called when the entity collides with the {@link Player}
+     *
+     * @param player The {@link Player} collecting the {@link Collectable}
+     */
+    void collected(Player player);
+
+}
