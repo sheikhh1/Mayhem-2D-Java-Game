35bb632e5acc0df194ead6da63493dd51e2678b1
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 14:41:08 +0000

    Fixed typing of SpawnPosition

--- src/me/mayhem/game/level/spawning/SpawnPosition.java
+++ src/me/mayhem/game/level/spawning/SpawnPosition.java
@@ -1,13 +1,17 @@
 package me.mayhem.game.level.spawning;
 
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
+
+import java.util.function.BiFunction;
 
 public class SpawnPosition {
 
     private final Vector position;
-    private final Runnable spawnType;
+    private final BiFunction<Vector, Level, Entity> spawnType;
 
-    public SpawnPosition(Vector position, Runnable spawnType) {
+    public SpawnPosition(Vector position, BiFunction<Vector, Level, Entity> spawnType) {
         this.position = position;
         this.spawnType = spawnType;
     }
@@ -16,7 +20,7 @@ public class SpawnPosition {
         return this.position;
     }
 
-    public Runnable getSpawnType() {
+    public BiFunction<Vector, Level, Entity> getSpawnType() {
         return this.spawnType;
     }
 }
