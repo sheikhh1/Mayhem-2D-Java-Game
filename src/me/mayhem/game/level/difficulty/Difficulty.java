78c82af87b00ce122c47431e9340ea662e04c59c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-17 14:48:59 +0000

    Changed Difficulty to use ImageLevelGenerator

--- src/me/mayhem/game/level/difficulty/Difficulty.java
+++ src/me/mayhem/game/level/difficulty/Difficulty.java
@@ -1,17 +1,15 @@
 package me.mayhem.game.level.difficulty;
 
 import me.mayhem.game.level.layout.ai.LevelGenerator;
-import me.mayhem.game.level.layout.ai.impl.DifficultLevelGenerator;
 import me.mayhem.game.level.layout.ai.impl.ImageLevelGenerator;
-import me.mayhem.game.level.layout.ai.impl.MediumLevelGenerator;
 import me.mayhem.game.level.spawning.EntitySpawner;
 import me.mayhem.game.level.spawning.impl.SimpleEntitySpawner;
 
 public enum Difficulty {
 
     EASY(new ImageLevelGenerator(), new SimpleEntitySpawner()),
-    MEDIUM(new MediumLevelGenerator(), new SimpleEntitySpawner()),
-    DIFFICULT(new DifficultLevelGenerator(), new SimpleEntitySpawner()),
+    MEDIUM(new ImageLevelGenerator(), new SimpleEntitySpawner()),
+    DIFFICULT(new ImageLevelGenerator(), new SimpleEntitySpawner()),
 
     ;
 
