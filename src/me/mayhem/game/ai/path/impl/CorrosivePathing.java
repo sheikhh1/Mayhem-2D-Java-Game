ad8e85b64958d153e3e3942623cf7f28832662de
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-18 15:50:51 +0000

    Copied move between blocks pathing for CorrosivePathing

---
+++ src/me/mayhem/game/ai/path/impl/CorrosivePathing.java
@@ -0,0 +1,41 @@
+package me.mayhem.game.ai.path.impl;
+
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.attribute.type.BooleanAttribute;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.state.EntityState;
+import me.mayhem.game.level.Level;
+
+public class CorrosivePathing implements Pathing {
+
+    private final Level currentLevel;
+
+    public CorrosivePathing(Level currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    @Override
+    public void updatePosition(Entity entity) {
+
+        if (entity.getState(1) == EntityState.BACK) {
+            entity.getMotion().add(-1, 0);
+        } else {
+            entity.getMotion().add(1, 0);
+        }
+
+        this.determineState(entity);
+    }
+
+    public void determineState(Entity entity) {
+        BooleanAttribute collidedLeft = (BooleanAttribute) entity.getAttribute("collidedLeft", Boolean.class);
+        BooleanAttribute collidedRight = (BooleanAttribute) entity.getAttribute("collidedRight", Boolean.class);
+
+        if (collidedLeft != null && collidedLeft.getValue()) {
+            entity.setState(EntityState.FORWARD);
+            collidedLeft.setValue(false);
+        } else if (collidedRight != null && collidedRight.getValue()) {
+            entity.setState(EntityState.BACK);
+            collidedRight.setValue(false);
+        }
+    }
+}
