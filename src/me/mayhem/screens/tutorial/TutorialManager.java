1391ee879593459c35e674b8d11193d068b16e3b
j-sammon <j.sammon@lancaster.ac.uk>
2021-02-18 13:11:31 +0000

    creating methods for buttons and sprites in the TutorialManager

--- src/me/mayhem/screens/tutorial/TutorialManager.java
+++ src/me/mayhem/screens/tutorial/TutorialManager.java
@@ -1,4 +1,70 @@
 package me.mayhem.screens.tutorial;
 
-public class TutorialManager {
+import me.mayhem.Mayhem;
+import me.mayhem.screens.ScreenManager;
+import me.mayhem.screens.tutorial.items.TutorialReturnButton;
+import me.mayhem.util.UtilSharedResources;
+import me.mayhem.util.Vector;
+import me.mayhem.util.ui.Interactable;
+import org.jsfml.audio.Sound;
+import org.jsfml.graphics.*;
+
+import java.util.ArrayList;
+
+public class TutorialManager implements ScreenManager {
+
+    private final Sound mainTheme;
+
+    private Interactable[] buttons;
+    private Sprite[] sprites;
+
+    public TutorialManager(RenderWindow window, Sound mainTheme){
+        this.mainTheme = mainTheme;
+    }
+    @Override
+    public void loadScreen(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void unloadScreen(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void draw(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void close(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public Sound getSound() {
+        return this.mainTheme;
+    }
+
+    private void createButtons(){
+        TutorialReturnButton returnButton = new TutorialReturnButton(createReturnButton());
+
+        this.buttons = new Interactable[]{returnButton};
+
+    }
+    private Shape createReturnButton(){
+        RectangleShape shape = new RectangleShape();
+
+        shape.setSize(new Vector(200, 100).toVector());
+        shape.setPosition((0), (Mayhem.SCREEN_HEIGHT - 100));
+        shape.setFillColor(new Color(176, 176, 176));
+
+        return shape;
+    }
+    private void createSprites() {
+        Sprite background = UtilSharedResources.getBackground();
+
+        this.sprites = new Sprite[]{background};
+    }
+
 }
