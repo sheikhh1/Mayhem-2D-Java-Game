59a1902cb99e63730b5f1c4c5a1326ad3ffb7e20
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-18 15:43:24 +0000

    Changed corrosive pathing

--- src/me/mayhem/game/entity/entities/enemies/corrosive/CorrosiveEnemy.java
+++ src/me/mayhem/game/entity/entities/enemies/corrosive/CorrosiveEnemy.java
@@ -1,6 +1,6 @@
 package me.mayhem.game.entity.entities.enemies.corrosive;
 
-import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.ai.path.impl.MoveBetweenBlocksPathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
@@ -15,7 +15,7 @@ public class CorrosiveEnemy extends Entity {
      * @param position   - Current Position of entity relative to the game window
      */
     public CorrosiveEnemy(Vector position, Level level) {
-        super(EntityType.CORROSIVE, position, Vector.getZero(), new SpriteHitbox(position, 0,0 ), Pathing.FORWARD_PATHING);
+        super(EntityType.CORROSIVE, position, Vector.getZero(), new SpriteHitbox(position, 0,0 ), new MoveBetweenBlocksPathing());
 
         this.setState(EntityState.FALLING);
     }
