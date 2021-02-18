5850fca66d3b54473031bf6bd8412145b505f784
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-18 15:44:35 +0000

    Implemented corrosive methods

--- src/me/mayhem/game/entity/entities/enemies/corrosive/CorrosiveEnemy.java
+++ src/me/mayhem/game/entity/entities/enemies/corrosive/CorrosiveEnemy.java
@@ -4,11 +4,17 @@ import me.mayhem.game.ai.path.impl.MoveBetweenBlocksPathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
+import me.mayhem.game.entity.entities.enemies.Enemy;
+import me.mayhem.game.entity.player.Player;
 import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
+import org.jsfml.system.Clock;
 
-public class CorrosiveEnemy extends Entity {
+public class CorrosiveEnemy extends Entity implements Enemy {
+
+    private final Clock attackedAnimateClock = new Clock();
+
     /**
      * Entity Constructor
      *
@@ -20,10 +26,38 @@ public class CorrosiveEnemy extends Entity {
         this.setState(EntityState.FALLING);
     }
 
-    @Override
     public void tick() {
         super.tick();
 
+        if (this.isBack()) {
+            this.animate.setRow(9);
+            this.animate.setPause(false);
+        } else if (this.isForward()){
+            this.animate.setRow(11);
+            this.animate.setPause(false);
+        }
+
+        if(this.isMelee() && this.getFacing().getX() == 1) {
+            this.animate.setAvailableFrames(6);
+            this.animate.setRow(15);
+            this.animate.setPause(false);
+        } else if (isMelee() && this.getFacing().getX() == -1) {
+            this.animate.setAvailableFrames(6);
+            this.animate.setRow(13);
+            this.animate.setPause(false);
+        }
+
+        if (this.attackedAnimateClock.getElapsedTime().asMilliseconds() > 100) {
+            this.setMelee(false);
+        }
+
         this.animate.setSpritePosition(this.getPosition().toVector());
+    }
+
+    @Override
+    public void attack(Player player) {
+        player.damage(this, 1);
+        this.setState(EntityState.MELEE);
+        this.attackedAnimateClock.restart();
     }
 }
