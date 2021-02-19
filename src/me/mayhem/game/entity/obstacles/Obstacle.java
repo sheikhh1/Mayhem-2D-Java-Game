bd677218f071858fcc357d8dcc01f304b401ae96
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-19 10:38:17 +0000

    Fixed merge errors

--- src/me/mayhem/game/entity/obstacles/Obstacle.java
+++ src/me/mayhem/game/entity/obstacles/Obstacle.java
@@ -4,7 +4,6 @@ import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.collision.impl.SpriteHitbox;
 import me.mayhem.game.entity.Entity;
 import me.mayhem.game.entity.EntityType;
-import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
 import me.mayhem.util.file.UtilSprite;
@@ -20,9 +19,6 @@ public class Obstacle extends Entity {
 
         sprite = UtilSprite.loadFromPath("obstacles/Spike.png");
         sprite.setPosition(position.toVector());
-
-        this.getEntityPhysics().setEntityMotion(this.getMotion());
-        this.setState(EntityState.NO_MOTION);
     }
 
     @Override
