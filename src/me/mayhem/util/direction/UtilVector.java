6dad62adfdd472d5226130aa2a8b17c8e7d3d910
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-19 13:36:48 +0000

    Added UtilVector#inSight

--- src/me/mayhem/util/direction/UtilVector.java
+++ src/me/mayhem/util/direction/UtilVector.java
@@ -1,10 +1,25 @@
 package me.mayhem.util.direction;
 
+import me.mayhem.game.entity.Entity;
 import me.mayhem.util.Vector;
 
 public class UtilVector {
 
     public static double getDistanceSquared(Vector first, Vector second) {
         return first.clone().subtract(second).getLengthSquared();
+    }
+
+    public static boolean inSight(Entity entity, Entity other) {
+        Vector betweenEntities = entity.getPosition().clone().subtract(other.getPosition()).normalize();
+
+        if (betweenEntities.getX() > 0 && entity.getFacing().getX() > 0) {
+            return true;
+        }
+
+        if (betweenEntities.getX() < 0 && entity.getFacing().getX() < 0) {
+            return true;
+        }
+
+        return false;
     }
 }
