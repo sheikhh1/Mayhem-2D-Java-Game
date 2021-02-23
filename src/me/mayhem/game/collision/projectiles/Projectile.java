df68d4dc9dbfc949bd7459f1a9d7253006dd9c05
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-23 13:18:39 +0000

    Refactored Projectile class

---
+++ src/me/mayhem/game/collision/projectiles/Projectile.java
@@ -0,0 +1,27 @@
+package me.mayhem.game.collision.projectiles;
+
+import me.mayhem.game.ai.path.Pathing;
+import me.mayhem.game.attribute.Attribute;
+import me.mayhem.game.collision.impl.SpriteHitbox;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.EntityType;
+import me.mayhem.util.Vector;
+import me.mayhem.util.file.UtilSprite;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Sprite;
+
+public class Projectile extends Entity {
+
+    private final Sprite sprite;
+
+    public Projectile(Vector position, Vector motion, String image, Attribute<?>... attributes) {
+        super(EntityType.PROJECTILE, position, motion, new SpriteHitbox(position, 5, 5), Pathing.NO_PATHING, attributes);
+
+        this.sprite = UtilSprite.loadFromPath(image);
+    }
+
+    @Override
+    public void update(RenderWindow window) {
+        window.draw(this.sprite);
+    }
+}
