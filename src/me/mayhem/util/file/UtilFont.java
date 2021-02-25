910410e6d15e4749833c045f27db5121a6ac9e06
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 10:56:36 +0000

    Implemented cache to UtilFont

--- src/me/mayhem/util/file/UtilFont.java
+++ src/me/mayhem/util/file/UtilFont.java
@@ -4,14 +4,25 @@ import me.mayhem.Mayhem;
 import org.jsfml.graphics.Font;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UtilFont {
 
+    private static final Map<String, Font> FONT_CACHE = new HashMap<>();
+
     public static Font loadFont(String font) {
+        Font cachedFont = FONT_CACHE.get(font);
+
+        if (cachedFont != null) {
+            return cachedFont;
+        }
+
         Font loadedFont = new Font();
 
         try {
             loadedFont.loadFromStream(Mayhem.class.getClassLoader().getResourceAsStream(font));
+            FONT_CACHE.put(font, loadedFont);
         } catch (IOException e) {
             e.printStackTrace();
         }
