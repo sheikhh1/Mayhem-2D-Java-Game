2584a4753329af0f8ab73eca4f3a965f8f09a492
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 12:34:13 +0000

    Refactored

--- src/me/mayhem/screens/losescreen/items/LoseNewGameButton.java
+++ src/me/mayhem/screens/losescreen/items/LoseNewGameButton.java
@@ -8,16 +8,17 @@ import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 public class LoseNewGameButton extends ButtonInteractable {
+    
     public LoseNewGameButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "Restart Level");
     }
+
     @Override
     protected void call(RenderWindow window, Event event) {
-        if (event.type == Event.Type.MOUSE_BUTTON_PRESSED){
-
-            LoseScreenManager screenManager = (LoseScreenManager) Mayhem.getCurrentScreen();
-
-
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
         }
+
+        LoseScreenManager screenManager = (LoseScreenManager) Mayhem.getCurrentScreen();
     }
 }
