b96aa9e14b6243703d9d0b884f58b459feced30c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 10:20:13 +0000

    Changed difficulty to no longer load level generator

--- src/me/mayhem/game/level/difficulty/Difficulty.java
+++ src/me/mayhem/game/level/difficulty/Difficulty.java
@@ -1,28 +1,20 @@
 package me.mayhem.game.level.difficulty;
 
-import me.mayhem.game.level.layout.ai.LevelGenerator;
-import me.mayhem.game.level.layout.ai.impl.ImageLevelGenerator;
 import me.mayhem.game.level.spawning.EntitySpawner;
 import me.mayhem.game.level.spawning.impl.SimpleEntitySpawner;
 
 public enum Difficulty {
 
-    EASY(new ImageLevelGenerator(), new SimpleEntitySpawner()),
-    MEDIUM(new ImageLevelGenerator(), new SimpleEntitySpawner()),
-    DIFFICULT(new ImageLevelGenerator(), new SimpleEntitySpawner()),
+    EASY(new SimpleEntitySpawner()),
+    MEDIUM(new SimpleEntitySpawner()),
+    DIFFICULT(new SimpleEntitySpawner()),
 
     ;
 
-    private final LevelGenerator generator;
     private final EntitySpawner spawner;
 
-    Difficulty(LevelGenerator generator, EntitySpawner spawner) {
-        this.generator = generator;
+    Difficulty(EntitySpawner spawner) {
         this.spawner = spawner;
-    }
-
-    public LevelGenerator getGenerator() {
-        return this.generator;
     }
 
     public EntitySpawner getSpawner() {
