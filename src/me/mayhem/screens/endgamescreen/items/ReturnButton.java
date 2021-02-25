bee2869f7ec892e86486efad51bdd084e4592e38
j-sammon <j.sammon@lancaster.ac.uk>
2021-02-25 17:36:40 +0000

    button to return you to homepage

---
+++ src/me/mayhem/screens/endgamescreen/items/ReturnButton.java
@@ -0,0 +1,12 @@
+package me.mayhem.screens.endgamescreen.items;
+
+import me.mayhem.util.ui.impl.ButtonInteractable;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.event.Event;
+
+public class ReturnButton extends ButtonInteractable {
+    @Override
+    protected void call(RenderWindow window, Event event) {
+
+    }
+}
