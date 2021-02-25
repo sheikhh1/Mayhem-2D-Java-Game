8b7ef74f69e8f274af8c0481ea8ac17473a17644
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 12:18:31 +0000

    Implemented WinScreenManager

--- src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
+++ src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
@@ -1,14 +1,14 @@
 package me.mayhem.game.entity.player.listeners.game;
 
-import me.mayhem.game.entity.Entity;
-import me.mayhem.game.entity.EntityType;
+import me.mayhem.Mayhem;
 import me.mayhem.game.entity.entities.collect.Collectable;
-import me.mayhem.game.entity.entities.collect.key.KeyCard;
 import me.mayhem.game.entity.entities.enemies.Enemy;
 import me.mayhem.game.entity.entities.friendly.door.Door;
 import me.mayhem.game.entity.event.PlayerCollideWithEntityEvent;
-import me.mayhem.game.event.struct.Event;
 import me.mayhem.game.event.struct.EventListener;
+import me.mayhem.screens.gamescreen.GameScreenManager;
+import me.mayhem.screens.winscreen.WinScreenManager;
+import org.jsfml.graphics.RenderWindow;
 
 public class PlayerEnemyCollideListener {
 
@@ -20,7 +20,11 @@ public class PlayerEnemyCollideListener {
             ((Collectable) event.getEntity()).collected(event.getPlayer());
         }else if (event.getEntity() instanceof Door){
             if (event.getPlayer().getInventory().contains("KeyCard")){
-                //todo
+                RenderWindow window = Mayhem.getMainWindow();
+                GameScreenManager currentScreen = (GameScreenManager) Mayhem.getCurrentScreen();
+
+                Mayhem.getCurrentScreen().unloadScreen(window);
+                Mayhem.setCurrentScreen(new WinScreenManager(window, Mayhem.getCurrentScreen().getSound(), currentScreen));
             }
         }
     }
