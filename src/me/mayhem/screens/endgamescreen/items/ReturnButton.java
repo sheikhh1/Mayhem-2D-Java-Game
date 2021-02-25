934589f2c5415a48c47ce8a9a182c575651c8244
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 20:53:25 +0000

    Refactor

--- src/me/mayhem/screens/endgamescreen/items/ReturnButton.java
+++ src/me/mayhem/screens/endgamescreen/items/ReturnButton.java
@@ -2,7 +2,6 @@ package me.mayhem.screens.endgamescreen.items;
 
 import me.mayhem.Mayhem;
 import me.mayhem.screens.endgamescreen.EndGameScreenManager;
-import me.mayhem.screens.escapescreen.EscapeScreenManager;
 import me.mayhem.screens.homepage.HomePageManager;
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
@@ -10,6 +9,7 @@ import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 public class ReturnButton extends ButtonInteractable {
+
     public ReturnButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "Homepage ");
     }
@@ -18,10 +18,11 @@ public class ReturnButton extends ButtonInteractable {
     protected void call(RenderWindow window, Event event) {
         EndGameScreenManager screen = (EndGameScreenManager) Mayhem.getCurrentScreen();
 
-        if (event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
-            screen.unloadScreen(window);
-            screen.getGameScreen().getGame().shutdownLevel();
-            Mayhem.setCurrentScreen(new HomePageManager(window));
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
         }
+
+        screen.unloadScreen(window);
+        Mayhem.setCurrentScreen(new HomePageManager(window));
     }
 }
