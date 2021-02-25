12f09c975d4fed2cce9768f6436d3fffb6409e0b
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 22:15:57 +0000

    Added build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+JAVA_VERSION=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }'`
+
+if [[ $JAVA_VERSION < "15.0.0" ]]
+ then
+   echo "Java version MUST be 15 or greater"
+   exit 0
+fi
+
+find -name "*.java" > sources.txt
+javac -d classes -cp libs/jsfml.jar @sources.txt
+javadoc -d docs -cp libs/jsfml.jar @sources.txt
