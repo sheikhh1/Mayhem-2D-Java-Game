d5175634c2d32b8dca1e55fadc334e7699873273
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 13:14:09 +0000

    Refactored

--- src/me/mayhem/screens/winscreen/items/WinContinueButton.java
+++ src/me/mayhem/screens/winscreen/items/WinContinueButton.java
@@ -1,22 +1,22 @@
 package me.mayhem.screens.winscreen.items;
 
-import me.mayhem.Mayhem;
-import me.mayhem.screens.escapescreen.EscapeScreenManager;
-import me.mayhem.screens.savescreen.SaveScreenManager;
-import me.mayhem.screens.winscreen.WinScreenManager;
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 public class WinContinueButton extends ButtonInteractable {
-    public WinContinueButton(Shape shape) {super(shape, "fonts/FreeSans.ttf", "continue");
+
+    public WinContinueButton(Shape shape) {
+        super(shape, "fonts/FreeSans.ttf", "continue");
     }
+
     @Override
     protected void call(RenderWindow window, Event event) {
-        if (!(Mayhem.getCurrentScreen() instanceof EscapeScreenManager)) {
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
             return;
         }
 
+        //TODO: open next level!
     }
 }
