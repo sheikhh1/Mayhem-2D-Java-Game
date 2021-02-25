c658f8a482601bf3defed23d8fe6ec71e14a7c86
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 11:13:50 +0000

    Refactored

--- src/me/mayhem/screens/loadpage/items/NextButton.java
+++ src/me/mayhem/screens/loadpage/items/NextButton.java
@@ -1,32 +1,27 @@
 package me.mayhem.screens.loadpage.items;
 
 import me.mayhem.Mayhem;
-import me.mayhem.screens.ScreenManager;
 import me.mayhem.screens.loadpage.LoadPageManager;
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
-import java.awt.*;
+public class NextButton extends ButtonInteractable {
 
-public class NextButton extends ButtonInteractable{
     public NextButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "next");
     }
 
     @Override
     protected void call(RenderWindow window, Event event) {
-        if ( event.type == Event.Type.MOUSE_BUTTON_PRESSED){
+        if (event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
             LoadPageManager screen = (LoadPageManager) Mayhem.getCurrentScreen();
-            if (screen.getSavepage() != screen.getMaxSavepages() ){
-                screen.setSavepage(screen.getSavepage()  + 1);
+
+            if (screen.getSavepage() != screen.getMaxSavepages()) {
+                screen.setSavepage(screen.getSavepage() + 1);
                 screen.setpagechange(true);
             }
-
-
         }
-
-
     }
 }
