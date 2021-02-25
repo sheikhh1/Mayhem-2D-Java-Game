3f7b5e6526511917ea5b62b3b2460114ba9c6fcf
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 12:07:52 +0000

    Added PlayerDeathListener

---
+++ src/me/mayhem/game/entity/listener/PlayerDeathListener.java
@@ -0,0 +1,31 @@
+package me.mayhem.game.entity.listener;
+
+import me.mayhem.Mayhem;
+import me.mayhem.game.entity.event.EntityDeathEvent;
+import me.mayhem.game.entity.player.Player;
+import me.mayhem.game.event.EventManager;
+import me.mayhem.game.event.struct.EventListener;
+import me.mayhem.screens.gamescreen.GameScreenManager;
+import me.mayhem.screens.losescreen.LoseScreenManager;
+import org.jsfml.graphics.RenderWindow;
+
+public class PlayerDeathListener {
+
+    public PlayerDeathListener() {
+        EventManager.registerListener(this);
+    }
+
+    @EventListener
+    public void onPlayerDeath(EntityDeathEvent event) {
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+
+        RenderWindow window = Mayhem.getMainWindow();
+        GameScreenManager currentScreen = (GameScreenManager) Mayhem.getCurrentScreen();
+
+        Mayhem.getCurrentScreen().unloadScreen(window);
+        currentScreen.getGame().shutdownLevel();
+        Mayhem.setCurrentScreen(new LoseScreenManager(window, Mayhem.getCurrentScreen().getSound(), currentScreen));
+    }
+}
