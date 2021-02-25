28933c0a77edb89add149c335eb36995e180851d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-25 11:47:26 +0000

    Refactored and cleaned up

---
+++ src/me/mayhem/screens/loadpage/items/LoadGameFileButton.java
@@ -0,0 +1,31 @@
+package me.mayhem.screens.loadpage.items;
+
+import me.mayhem.Mayhem;
+import me.mayhem.screens.gamescreen.GameScreenManager;
+import me.mayhem.screens.loadpage.SaveData;
+import me.mayhem.util.ui.impl.ButtonInteractable;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Shape;
+import org.jsfml.window.event.Event;
+
+
+public class LoadGameFileButton extends ButtonInteractable {
+
+    private final SaveData loadFile;
+
+    public LoadGameFileButton(Shape shape, SaveData loadFile) {
+        super(shape, "fonts/FreeSans.ttf", loadFile.getName());
+
+        this.loadFile = loadFile;
+    }
+
+    @Override
+    protected void call(RenderWindow window, Event event) {
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
+        }
+
+        Mayhem.getCurrentScreen().unloadScreen(window);
+        Mayhem.setCurrentScreen(new GameScreenManager(window, this.loadFile.getDifficulty(), this.loadFile.getId(), this.loadFile.getName()));
+    }
+}
