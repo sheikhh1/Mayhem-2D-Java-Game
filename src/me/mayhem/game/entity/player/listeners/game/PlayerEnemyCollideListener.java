7b48c52f8f80f9c21139d3e0aa5c82778f5a31bf
j-sammon <j.sammon@lancaster.ac.uk>
2021-02-25 15:32:37 +0000

    creating the listner for the collision between the projectile and the player

--- src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
+++ src/me/mayhem/game/entity/player/listeners/game/PlayerEnemyCollideListener.java
@@ -2,6 +2,7 @@ package me.mayhem.game.entity.player.listeners.game;
 
 import me.mayhem.game.entity.entities.collect.Collectable;
 import me.mayhem.game.entity.entities.enemies.Enemy;
+import me.mayhem.game.entity.entities.projectiles.Projectile;
 import me.mayhem.game.entity.event.PlayerCollideWithEntityEvent;
 import me.mayhem.game.event.struct.EventListener;
 
@@ -13,6 +14,8 @@ public class PlayerEnemyCollideListener {
             ((Enemy) event.getEntity()).attack(event.getPlayer());
         } else if (event.getEntity() instanceof Collectable) {
             ((Collectable) event.getEntity()).collected(event.getPlayer());
+        } else if (event.getEntity() instanceof Projectile){
+            //todo
         }
     }
 }
