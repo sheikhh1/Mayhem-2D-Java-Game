55a58414039eb80fccc6145b1bdbecd7c3f7d40a
j-sammon <j.sammon@lancaster.ac.uk>
2021-02-25 17:36:18 +0000

    created a screen for when you win all the levels

---
+++ src/me/mayhem/screens/endgamescreen/EndGameScreenManager.java
@@ -0,0 +1,33 @@
+package me.mayhem.screens.endgamescreen;
+
+import me.mayhem.screens.ScreenManager;
+import org.jsfml.audio.Sound;
+import org.jsfml.graphics.RenderWindow;
+
+public class EndGameScreenManager implements ScreenManager {
+
+    @Override
+    public void loadScreen(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void unloadScreen(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void draw(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public void close(RenderWindow renderWindow) {
+
+    }
+
+    @Override
+    public Sound getSound() {
+        return null;
+    }
+}
