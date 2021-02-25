9c1856448b438551bc2edc26d82099fd77b0a542
j-sammon <j.sammon@lancaster.ac.uk>
2021-02-25 18:31:50 +0000

    creating constructor

--- src/me/mayhem/screens/endgamescreen/items/ReturnButton.java
+++ src/me/mayhem/screens/endgamescreen/items/ReturnButton.java
@@ -2,9 +2,13 @@ package me.mayhem.screens.endgamescreen.items;
 
 import me.mayhem.util.ui.impl.ButtonInteractable;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 public class ReturnButton extends ButtonInteractable {
+    public ReturnButton(Shape shape) {
+        super(shape, "fonts/FreeSans.ttf", "Homepage ");
+    }
     @Override
     protected void call(RenderWindow window, Event event) {
 
