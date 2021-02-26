4ffa209b98ce9675b5e59e5086f69d58b4923090
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 14:29:07 +0000

    Javadocs

--- src/me/mayhem/save/SaveData.java
+++ src/me/mayhem/save/SaveData.java
@@ -8,7 +8,10 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- * a class used to store data about the current data of the gamemanger
+ *
+ * A Data Transfer Object used for storing the relevant data about a save, and it's file.
+ * Majority immutable to improve memory performance but the id must allow to change as for level completion
+ *
  */
 public class SaveData {
 
@@ -20,9 +23,12 @@ public class SaveData {
 
     /**
      *
-     * @param file the file to be saveed in
-     * @param name the name of the player
-     * @param id the id of the level
+     * Default constructor for making the DTO. Private as to ensure all instances are created using
+     * {@link SaveData#from(File)} so it can load directly from a given file
+     *
+     * @param file the file the data is loaded from and saved in
+     * @param name the name of the player and the file
+     * @param id the id of the level the player is currently at
      * @param difficulty the difficulty level of the game
      */
     private SaveData(File file, String name, int id, Difficulty difficulty) {
@@ -32,30 +38,63 @@ public class SaveData {
         this.difficulty = difficulty;
     }
 
+    /**
+     *
+     * Gets the file the level is stored in
+     *
+     * @return The storage
+     */
     public File getFile() {
         return this.file;
     }
 
+    /**
+     *
+     * The name of the file and the player (without the .txt suffix)
+     *
+     * @return The user's chosen name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     *
+     * The id of the current level the player is playing
+     *
+     * @return {@link me.mayhem.game.level.Level} id
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     *
+     * Updates the id of the current level
+     *
+     * @param id {@link me.mayhem.game.level.Level} id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     *
+     * Gets the difficulty of the game the user selected
+     *
+     * @return The difficulty selected at the start of the creation of this save
+     */
     public Difficulty getDifficulty() {
         return this.difficulty;
     }
 
     /**
      *
-     * @param file the file to be written from
-     * @return the data object that has been written
+     * Loads and instance of this DTO from the given file
+     *
+     * @param file the file to be read from
+     * @return the data object that has been read
      */
     public static SaveData from(File file) {
         String name = null;
@@ -83,10 +122,12 @@ public class SaveData {
 
     /**
      *
-     * @param name the name of the player
+     * Creates an instance of the DTO for a level that has yet to have a file created for it
+     *
+     * @param name the name of the player and the file
      * @param id the id of the level
      * @param difficulty  the difficulty of the level
-     * @return a new savedata object containing the data of the level
+     * @return a new instance containing the game's save data
      */
     public static SaveData of(String name, int id, Difficulty difficulty) {
         return new SaveData(new File(SaveFileManager.SAVE_DIR + File.separator + name + ".txt"), name, id, difficulty);
