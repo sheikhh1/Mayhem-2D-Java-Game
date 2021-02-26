60ab8dfcc6fffabcf1d603ed2f392c6d678e8dba
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 05:51:23 +0000

    Adding vaccine type

--- src/me/mayhem/game/entity/player/vaccine/VaccineType.java
+++ src/me/mayhem/game/entity/player/vaccine/VaccineType.java
@@ -1,4 +1,21 @@
 package me.mayhem.game.entity.player.vaccine;
 
+import me.mayhem.util.file.UtilImageLoader;
+import org.jsfml.graphics.Texture;
+
 public enum VaccineType {
+
+    LEFT(1, "projectiles/LeftSyringe.png"),
+    RIGHT(1, "projectiles/RightSyringe.png")
+    ;
+
+    private final int damage;
+    private final Texture imageTexture;
+    private final String imagePath;
+    
+    VaccineType(int damage, String imagePath) {
+        this.damage = damage;
+        this.imageTexture = UtilImageLoader.loadTextureFromStream(imagePath);
+        this.imagePath = imagePath;
+    }
 }
