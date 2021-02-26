f58e0307359023a555efc3eaebf08955c42049c7
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 04:39:20 +0000

    Created Vaccine class

---
+++ src/me/mayhem/game/entity/player/vaccine/Vaccine.java
@@ -0,0 +1,11 @@
+package me.mayhem.game.entity.player.vaccine;
+
+import me.mayhem.game.attribute.Attribute;
+import me.mayhem.game.entity.Entity;
+import me.mayhem.util.Vector;
+
+public class Vaccine extends Entity {
+    public Vaccine(Vector position, Attribute<?>... attributes) {
+
+    }
+}
