62b9cc21f31f063c306ad2e4c481489e00d05456
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 04:21:28 +0000

    Added different projectile types

--- src/me/mayhem/game/entity/entities/projectile/ProjectileType.java
+++ src/me/mayhem/game/entity/entities/projectile/ProjectileType.java
@@ -4,7 +4,12 @@ import me.mayhem.util.file.UtilImageLoader;
 import org.jsfml.graphics.Texture;
 
 public enum ProjectileType {
-    
+
+    WEAK(1, 1, "projectiles/greenfireball.png"),
+    NORMAL(2, 2, "projectiles/bluefireball.png"),
+    STRONG(3, 3, "projectiles/orangefireball.PNG"),
+    SUPER(4, 5, "projectiles/redfireball.PNG")
+    ;
 
     private final int index;
     private final int damage;
