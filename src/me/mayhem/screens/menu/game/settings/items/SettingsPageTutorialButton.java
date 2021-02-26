9922eb1eb1cc456e23c17c069fccb93669c890a9
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 02:48:58 +0000

    Javadocs

--- src/me/mayhem/screens/menu/game/settings/items/SettingsPageTutorialButton.java
+++ src/me/mayhem/screens/menu/game/settings/items/SettingsPageTutorialButton.java
@@ -8,17 +8,24 @@ import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 /**
- * a button that lets you select the tutorial for the game
+ *
+ * Concrete implementation of {@link me.mayhem.util.ui.Interactable} and {@link ButtonInteractable} classes for the tutorial
+ * button for the {@link me.mayhem.screens.menu.game.settings.NewGameSettingsPageManager} to take the user to the {@link TutorialManager}
+ *
  */
 public class SettingsPageTutorialButton extends ButtonInteractable {
+
     public SettingsPageTutorialButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "?");
     }
+
     @Override
     protected void call(RenderWindow window, Event event) {
-        if ( event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
-            Mayhem.getCurrentScreen().unloadScreen(window);
-            Mayhem.setCurrentScreen(new TutorialManager(window, Mayhem.getCurrentScreen().getSound()));
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
         }
+
+        Mayhem.getCurrentScreen().unloadScreen(window);
+        Mayhem.setCurrentScreen(new TutorialManager(window, Mayhem.getCurrentScreen().getSound()));
     }
 }
