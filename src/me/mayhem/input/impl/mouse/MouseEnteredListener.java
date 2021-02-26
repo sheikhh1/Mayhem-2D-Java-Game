e6b75ad927dfdfd20b0289f230a7f10f5f80d9cb
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 14:43:40 +0000

    Javadocs

--- src/me/mayhem/input/impl/mouse/MouseEnteredListener.java
+++ src/me/mayhem/input/impl/mouse/MouseEnteredListener.java
@@ -4,8 +4,21 @@ import me.mayhem.input.InputListener;
 import org.jsfml.window.event.Event;
 import org.jsfml.window.event.MouseEvent;
 
+/**
+ *
+ * Concrete implementation of the {@link InputListener} to remove any generics from higher level code
+ * thus making it easier for the team to use listeners as all the potential ambiguity and confusion is removed.
+ *
+ * This implementation is for the event where the user moves their mouse into a specific area
+ *
+ */
 public abstract class MouseEnteredListener extends InputListener<MouseEvent> {
 
+    /**
+     *
+     * Default constructor passing the correct {@link Event.Type} that relate to this listener
+     *
+     */
     public MouseEnteredListener() {
         super(Event.Type.MOUSE_ENTERED);
     }
