5befc720e911407b4abd902bb1ff059113db72ea
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 02:42:23 +0000

    Javadocs

--- src/me/mayhem/screens/menu/home/items/HomePageLoadButton.java
+++ src/me/mayhem/screens/menu/home/items/HomePageLoadButton.java
@@ -7,7 +7,14 @@ import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
+/**
+ *
+ * Concrete implementation of {@link me.mayhem.util.ui.Interactable} and {@link ButtonInteractable} classes for the quit
+ * button for the {@link me.mayhem.screens.menu.home.HomePageManager} to take the user to the {@link LoadPageManager}
+ *
+ */
 public class HomePageLoadButton extends ButtonInteractable {
+
     /**
      * the button that lets you go the loading page
      *
@@ -19,9 +26,11 @@ public class HomePageLoadButton extends ButtonInteractable {
 
     @Override
     protected void call(RenderWindow window, Event event) {
-        if (event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
-            Mayhem.getCurrentScreen().unloadScreen(window);
-            Mayhem.setCurrentScreen(new LoadPageManager(window, Mayhem.getCurrentScreen().getSound()));
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
         }
+
+        Mayhem.getCurrentScreen().unloadScreen(window);
+        Mayhem.setCurrentScreen(new LoadPageManager(window, Mayhem.getCurrentScreen().getSound()));
     }
 }
