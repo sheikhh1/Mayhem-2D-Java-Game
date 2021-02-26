374cbfeb60df864e518f86d3773c225a9ec81ef7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 14:40:39 +0000

    Javadocs

--- src/me/mayhem/input/impl/screen/GainedFocusListener.java
+++ src/me/mayhem/input/impl/screen/GainedFocusListener.java
@@ -3,8 +3,21 @@ package me.mayhem.input.impl.screen;
 import me.mayhem.input.InputListener;
 import org.jsfml.window.event.Event;
 
+/**
+ *
+ * Concrete implementation of the {@link InputListener} to remove any generics from higher level code
+ * thus making it easier for the team to use listeners as all the potential ambiguity and confusion is removed.
+ *
+ * This implementation is for the event where the user clicks on to the screen
+ *
+ */
 public abstract class GainedFocusListener extends InputListener<Event> {
 
+    /**
+     *
+     * Default constructor passing the correct {@link Event.Type} that relate to this listener
+     *
+     */
     public GainedFocusListener() {
         super(Event.Type.GAINED_FOCUS);
     }
