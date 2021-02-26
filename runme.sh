25fdb6509d014d413c13a6bd15bf153b3da06ab0
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 16:11:35 +0000

    Fixing scripts

--- runme.sh
+++ runme.sh
@@ -7,4 +7,4 @@ if [[ $JAVA_VERSION < "11.0.0" ]]
    exit 0
 fi
 
-java -Xmx 500m -cp resources:classes:libs/jsfml.jar me.mayhem.Mayhem
+java -Xmx 100m -cp resources:classes:libs/jsfml.jar me.mayhem.Mayhem
