22cd0df9ad7787f1045bee92d4931a2ececfb7bc
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 14:40:22 +0000

    Javadocs

--- src/me/mayhem/input/impl/screen/LostFocusListener.java
+++ src/me/mayhem/input/impl/screen/LostFocusListener.java
@@ -3,8 +3,21 @@ package me.mayhem.input.impl.screen;
 import me.mayhem.input.InputListener;
 import org.jsfml.window.event.Event;
 
+/**
+ *
+ * Concrete implementation of the {@link InputListener} to remove any generics from higher level code
+ * thus making it easier for the team to use listeners as all the potential ambiguity and confusion is removed.
+ *
+ * This implementation is for the event where the user clicks out of the screen
+ *
+ */
 public abstract class LostFocusListener extends InputListener<Event> {
 
+    /**
+     *
+     * Default constructor passing the correct {@link Event.Type} that relate to this listener
+     *
+     */
     public LostFocusListener() {
         super(Event.Type.LOST_FOCUS);
     }
