d50e1c3e6f68c883e880be7191ef7bb06817d510
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 02:35:55 +0000

    Added functionality, need to add motion

--- src/me/mayhem/game/entity/entities/projectile/Projectile.java
+++ src/me/mayhem/game/entity/entities/projectile/Projectile.java
@@ -9,6 +9,7 @@ import me.mayhem.game.entity.entities.enemies.Enemy;
 import me.mayhem.game.entity.player.Player;
 import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.util.Vector;
+import me.mayhem.util.direction.Direction;
 import me.mayhem.util.file.UtilSprite;
 import org.jsfml.graphics.CircleShape;
 import org.jsfml.graphics.RenderWindow;
@@ -17,11 +18,19 @@ import org.jsfml.graphics.Sprite;
 public class Projectile extends Entity implements Enemy {
 
     private final Sprite sprite;
+    private int xMotion = 0;
 
-    public Projectile(Vector position, String image, Attribute<?>... attributes) {
+    public Projectile(Vector position,Vector playerPosition Attribute<?>... attributes) {
         super(EntityType.PROJECTILE, position, Vector.getZero(), new SpriteHitbox(position, 5, 5), Pathing.NO_PATHING, attributes);
 
-        this.sprite = UtilSprite.loadFromPath(image);
+        this.sprite = UtilSprite.loadFromPath(EntityType.PROJECTILE.getImagePath());
+
+        if (Direction.LEFT.is(playerPosition, position)) {
+            this.xMotion = -1;
+        } else {
+            this.xMotion = 1;
+        }
+
     }
 
     @Override
