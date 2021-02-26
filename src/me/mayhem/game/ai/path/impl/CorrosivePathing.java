723240e4bda0e5f316bfe4a577811dc261031f39
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 02:27:48 +0000

    Shoot projectile method added

--- src/me/mayhem/game/ai/path/impl/CorrosivePathing.java
+++ src/me/mayhem/game/ai/path/impl/CorrosivePathing.java
@@ -3,14 +3,17 @@ package me.mayhem.game.ai.path.impl;
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.attribute.type.BooleanAttribute;
 import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.entities.projectile.Projectile;
 import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
 import me.mayhem.util.direction.UtilVector;
+import org.jsfml.system.Clock;
 
 public class CorrosivePathing implements Pathing {
 
     private final Level currentLevel;
+    private Clock rateOfFire = new Clock();
 
     public CorrosivePathing(Level currentLevel) {
         this.currentLevel = currentLevel;
@@ -38,6 +41,18 @@ public class CorrosivePathing implements Pathing {
 
         entity.getAnimation().setTimeOut(Integer.MAX_VALUE);
         this.determineState(entity);
+    }
+
+    public void shootProjectile(Entity entity) {
+        Projectile fireBall;
+
+        if (entity.isEntityGrounded()) {
+            if (this.rateOfFire.getElapsedTime().asMilliseconds() > 1000) {
+                fireBall = new Projectile(entity.getPosition().clone(), this.currentLevel.getPlayer().getPosition());
+                this.currentLevel.spawnProjectile(fireBall);
+                this.rateOfFire.restart();
+            }
+        }
     }
 
     public void determineState(Entity entity) {
