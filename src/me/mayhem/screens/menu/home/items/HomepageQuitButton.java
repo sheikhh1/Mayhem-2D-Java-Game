335f80b1d54932498101f107c2d3bfd59f056e88
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 02:41:21 +0000

    Javadocs

--- src/me/mayhem/screens/menu/home/items/HomepageQuitButton.java
+++ src/me/mayhem/screens/menu/home/items/HomepageQuitButton.java
@@ -6,17 +6,31 @@ import org.jsfml.graphics.RenderWindow;
 import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
+/**
+ *
+ * Concrete implementation of {@link me.mayhem.util.ui.Interactable} and {@link ButtonInteractable} classes for the quit
+ * button for the {@link me.mayhem.screens.menu.home.HomePageManager} to quit the game
+ *
+ */
 public class HomepageQuitButton extends ButtonInteractable {
 
+    /**
+     *
+     * Constructor taking the background shape
+     *
+     * @param shape the background shape
+     */
     public HomepageQuitButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "Quit");
     }
 
     @Override
     protected void call(RenderWindow window, Event event) {
-        if (event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
-            Mayhem.getCurrentScreen().unloadScreen(window);
-            window.close();
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
         }
+
+        Mayhem.getCurrentScreen().unloadScreen(window);
+        window.close();
     }
 }
