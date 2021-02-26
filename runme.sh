800511b6156d241257449001a4e70004ac96eef5
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 15:01:05 +0000

    Fixed java versions

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 JAVA_VERSION=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }'`
 
-if [[ $JAVA_VERSION < "15.0.0" ]]
+if [[ $JAVA_VERSION < "11.0.0" ]]
  then
    echo "Java version MUST be 15 or greater"
    exit 0
