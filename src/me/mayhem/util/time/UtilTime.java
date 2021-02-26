aee362bf2ee0688f74ea78c263d48110fe67b472
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 01:34:36 +0000

    Javadocs

--- src/me/mayhem/util/time/UtilTime.java
+++ src/me/mayhem/util/time/UtilTime.java
@@ -2,12 +2,26 @@ package me.mayhem.util.time;
 
 import java.time.Duration;
 
+/**
+ *
+ * A static utility class for handling converting a millis time into a formatted text
+ *
+ */
 public class UtilTime {
 
+    private static final String FORMAT = "%d:%02d:%02d";
+
+    /**
+     *
+     * Converts the specified difference in milliseconds to hours, minutes, and seconds using the {@link UtilTime#FORMAT}
+     *
+     * @param difference The difference in milliseconds
+     * @return The formatted time
+     */
     public static String formatTime(long difference) {
         Duration duration = Duration.ofMillis(difference);
 
-        return String.format("%d:%02d:%02d",
+        return String.format(FORMAT,
                 duration.toHours(),
                 duration.toMinutesPart(),
                 duration.toSecondsPart());
