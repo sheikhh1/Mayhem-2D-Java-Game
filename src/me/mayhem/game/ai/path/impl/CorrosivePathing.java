9cbf17f007e549fece0c361d7d4f9723d0566644
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 01:39:06 +0000

    Updated to create new projectiles using a timer

--- src/me/mayhem/game/ai/path/impl/CorrosivePathing.java
+++ src/me/mayhem/game/ai/path/impl/CorrosivePathing.java
@@ -3,14 +3,18 @@ package me.mayhem.game.ai.path.impl;
 import me.mayhem.game.ai.path.Pathing;
 import me.mayhem.game.attribute.type.BooleanAttribute;
 import me.mayhem.game.entity.Entity;
+import me.mayhem.game.entity.entities.projectiles.Projectile;
 import me.mayhem.game.entity.state.EntityState;
 import me.mayhem.game.level.Level;
 import me.mayhem.util.Vector;
 import me.mayhem.util.direction.UtilVector;
+import org.jsfml.system.Clock;
 
 public class CorrosivePathing implements Pathing {
 
     private final Level currentLevel;
+    private Projectile shoot;
+    private Clock shootClock = new Clock();
 
     public CorrosivePathing(Level currentLevel) {
         this.currentLevel = currentLevel;
@@ -22,15 +26,19 @@ public class CorrosivePathing implements Pathing {
             return;
         }
 
+        System.out.println(shootClock.getElapsedTime().asMilliseconds());
+
         if (UtilVector.getDistanceSquared(entity.getPosition(), this.currentLevel.getPlayer().getPosition()) <= 60000) {
             Vector toPlayer = entity.getPosition().clone().subtract(this.currentLevel.getPlayer().getPosition()).normalize();
 
             if (toPlayer.getX() < 0) {
                 entity.getAnimation().setRow(11);
                 entity.setState(EntityState.STANDING);
+                this.shootProjectile(entity);
             } else {
                 entity.getAnimation().setRow(9);
                 entity.setState(EntityState.STANDING);
+                this.shootProjectile(entity);
             }
 
             return;
@@ -38,6 +46,16 @@ public class CorrosivePathing implements Pathing {
 
         entity.getAnimation().setTimeOut(Integer.MAX_VALUE);
         this.determineState(entity);
+    }
+
+    public void shootProjectile(Entity entity) {
+        if (entity.isEntityGrounded()) {
+            if (this.shootClock.getElapsedTime().asMilliseconds() > 1000) {
+                shoot = new Projectile(entity.getPosition().clone(), this.currentLevel.getPlayer().getPosition());
+                currentLevel.spawnProjectile(shoot);
+                this.shootClock.restart();
+            }
+        }
     }
 
     public void determineState(Entity entity) {
