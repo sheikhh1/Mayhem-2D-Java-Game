d8b47a80476fc5ba9b548df3909a87e4a6514a70
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 01:32:31 +0000

    Javadocs

--- src/me/mayhem/util/ui/impl/ButtonInteractable.java
+++ src/me/mayhem/util/ui/impl/ButtonInteractable.java
@@ -9,11 +9,39 @@ import org.jsfml.system.Vector2f;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ * An abstract implementation of the {@link me.mayhem.util.ui.Interactable} interface and the {@link me.mayhem.util.ui.AbstractMouseInteractable}
+ * that handles when the button is clicked by checking the click is in the area and passing it down to the concrete
+ * implementation of the class
+ *
+ * It handles centering the text both vertically and horizontally
+ *
+ */
 public abstract class ButtonInteractable extends AbstractMouseInteractable {
 
-    private  Text[] text;
+    private Text[] text;
     private final Font font;
 
+    /**
+     * Second constructor that converts the specified string of the font to the JSFML {@link Font}
+     *
+     * @param shape The shape background
+     * @param font  The font to be converted
+     * @param text  The text to be written
+     */
+    public ButtonInteractable(Shape shape, String font, String... text) {
+        this(shape, UtilFont.loadFont(font), text);
+    }
+
+    /**
+     * Default constructor that takes a JSFML {@link Shape} as the background image and hitbox. It takes a font to display
+     * the text in and a vararg of text
+     *
+     * @param shape The background shape
+     * @param font  The font to be written in
+     * @param text  The text to be written
+     */
     public ButtonInteractable(Shape shape, Font font, String... text) {
         super(shape);
 
@@ -21,6 +49,12 @@ public abstract class ButtonInteractable extends AbstractMouseInteractable {
         this.text = this.createText(text);
     }
 
+    /**
+     * Creates an {@link Text} array cache of the written text with the specified {@link Font}
+     *
+     * @param text The strings to be converted to the {@link Text}
+     * @return the {@link Text} cache
+     */
     private Text[] createText(String[] text) {
         List<Text> lines = new ArrayList<>();
         int yAxisDifferential = (int) (super.shape.getLocalBounds().height / (text.length + 1));
@@ -43,10 +77,6 @@ public abstract class ButtonInteractable extends AbstractMouseInteractable {
         return lines.toArray(new Text[0]);
     }
 
-    public ButtonInteractable(Shape shape, String font, String... text) {
-        this(shape, UtilFont.loadFont(font), text);
-    }
-
     @Override
     public void draw(RenderWindow renderWindow) {
         renderWindow.draw(super.shape);
@@ -61,8 +91,14 @@ public abstract class ButtonInteractable extends AbstractMouseInteractable {
         this.shape.setPosition(vector.toVector());
     }
 
-    public  void setText(String... filename){
-        this.text = this.createText(filename);
+    /**
+     *
+     * Updated the written text to the specified value
+     *
+     * @param text the new written text
+     */
+    public void setText(String... text) {
+        this.text = this.createText(text);
     }
 }
 
