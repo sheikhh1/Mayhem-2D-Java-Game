dca9628b62614033d6e639b0912d0057cb99e02d
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 02:50:00 +0000

    Javadocs

--- src/me/mayhem/screens/menu/game/settings/items/SettingsPageReturnButton.java
+++ src/me/mayhem/screens/menu/game/settings/items/SettingsPageReturnButton.java
@@ -8,18 +8,30 @@ import org.jsfml.graphics.Shape;
 import org.jsfml.window.event.Event;
 
 /**
- * a button to return you to the main page
+ *
+ * Concrete implementation of {@link me.mayhem.util.ui.Interactable} and {@link ButtonInteractable} classes for the return
+ * button for the {@link me.mayhem.screens.menu.game.settings.NewGameSettingsPageManager} to take the user to the {@link HomePageManager}
+ *
  */
 public class SettingsPageReturnButton extends ButtonInteractable {
+
+    /**
+     *
+     * Default constructor taking the shape as the background
+     *
+     * @param shape Background shape
+     */
     public SettingsPageReturnButton(Shape shape) {
         super(shape, "fonts/FreeSans.ttf", "Return");
     }
 
     @Override
     protected void call(RenderWindow window, Event event) {
-        if ( event.type == Event.Type.MOUSE_BUTTON_PRESSED) {
-            Mayhem.getCurrentScreen().unloadScreen(window);
-            Mayhem.setCurrentScreen(new HomePageManager(window, Mayhem.getCurrentScreen().getSound()));
+        if (event.type != Event.Type.MOUSE_BUTTON_PRESSED) {
+            return;
         }
+
+        Mayhem.getCurrentScreen().unloadScreen(window);
+        Mayhem.setCurrentScreen(new HomePageManager(window, Mayhem.getCurrentScreen().getSound()));
     }
 }
