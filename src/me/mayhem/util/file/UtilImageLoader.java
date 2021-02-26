7bcda796f053dac57d1983fe578090aed8a60973
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 01:50:25 +0000

    Deleted unused

--- src/me/mayhem/util/file/UtilImageLoader.java
+++ src/me/mayhem/util/file/UtilImageLoader.java
@@ -5,7 +5,6 @@ import org.jsfml.graphics.Texture;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
 
 /**
  *
@@ -13,18 +12,6 @@ import java.nio.file.Path;
  *
  */
 public class UtilImageLoader {
-
-    public static Image loadImageFromPath(Path path) {
-        Image image = new Image();
-
-        try {
-            image.loadFromFile(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return image;
-    }
 
     public static Image loadImageFromStream(InputStream inputStream) {
         Image image = new Image();
