b38b33dc20b4552cf4e56017bdad55a383374b51
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-26 03:05:32 +0000

    Javadocs

--- src/me/mayhem/screens/menu/game/naming/items/InputBox.java
+++ src/me/mayhem/screens/menu/game/naming/items/InputBox.java
@@ -4,10 +4,17 @@ import me.mayhem.util.ui.impl.TextAreaInteractable;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.system.Vector2f;
 
+/**
+ *
+ * The input box for the user to write into
+ *
+ */
 public class InputBox extends TextAreaInteractable {
+
     /**
      * a box that can be used to input text into the game
-     * @param position
+     *
+     * @param position the position of the input box
      */
     public InputBox(Vector2f position) {
         super(new RectangleShape(position), "fonts/FreeSans.ttf");
