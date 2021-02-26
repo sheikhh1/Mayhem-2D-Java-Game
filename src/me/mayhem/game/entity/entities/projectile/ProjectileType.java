aa2bb33bae2a53262cd4b2f4698b8373f85a66c5
sheikhh1 <haarissheikh2000@gmail.com>
2021-02-26 04:14:59 +0000

    Added getDamage method

--- src/me/mayhem/game/entity/entities/projectile/ProjectileType.java
+++ src/me/mayhem/game/entity/entities/projectile/ProjectileType.java
@@ -1,23 +1,33 @@
 package me.mayhem.game.entity.entities.projectile;
 
+import me.mayhem.util.file.UtilImageLoader;
+import org.jsfml.graphics.Texture;
+
 public enum ProjectileType {
 
-    
+
 
 
     private final int index;
     private final int damage;
     private final String imagePath;
+    private final Texture projectileTexture;
 
     ProjectileType(int index, int damage, String imagePath) {
         this.index = index;
         this.damage = damage;
+        this.projectileTexture = UtilImageLoader.loadTextureFromStream(imagePath);
         this.imagePath = imagePath;
     }
 
     private int getIndex() {
         return this.index;
     }
+
+    private int getDamage() {
+        return this.damage;
+    }
+
 
 
 }
